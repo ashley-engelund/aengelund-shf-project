141e35249878b26adea3bd61808b8628f5f4548a
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-14 22:14:12 -0800

    use class to check policy for parameters if no instance exists; also authorize for :update
    revert to original logic in the create action.  Since no instance of a membership_application will exist when the parameters are checked, use the MembershipApplication class to check the authorization policy

--- app/controllers/membership_applications_controller.rb
+++ app/controllers/membership_applications_controller.rb
@@ -1,15 +1,13 @@
 class MembershipApplicationsController < ApplicationController
 before_action :get_membership_application, only: [:show, :edit, :update]
-before_action :authorize_membership_application, only: [:show, :edit]
+before_action :authorize_membership_application, only: [ :update, :show, :edit]
+
   def new
     @membership_application = MembershipApplication.new
   end
 
   def create
-    # must create the membership_application before using the policy to check the parameters. @see https://www.sitepoint.com/straightforward-rails-authorization-with-pundit/
-    @membership_application = current_user.membership_applications.new
-    @membership_application.update(permitted_attributes(@membership_application))
-
+    @membership_application = current_user.membership_applications.new(membership_application_params)
     if @membership_application.save
       flash[:notice] = 'Thank you, Your application has been submitted'
       redirect_to root_path
@@ -46,7 +44,7 @@ before_action :authorize_membership_application, only: [:show, :edit]
 
   private
   def membership_application_params
-    params.require(:membership_application).permit(policy(@membership_application).permitted_attributes)
+    params.require(:membership_application).permit(*policy(@membership_application || MembershipApplication).permitted_attributes)
   end
 
   def get_membership_application
