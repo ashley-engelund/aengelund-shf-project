71e1012251c5b1aed46f0de6bd73ee27313a12fb
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-17 23:48:03 -0800

    (wip) upload steps: content that should (or should not) be on a page. Still to do: check that a file has been uploaded

--- features/step_definitions/upload_steps.rb
+++ features/step_definitions/upload_steps.rb
@@ -1,23 +1,23 @@
 
 
-When(/^I choose a file named "([^"]*)" to upload$/) do |arg|
-  attach_file  # TODO attach to what field, path to the file
-  # TODO path to the file = from config/test.rb
-
+When(/^I choose a file named "([^"]*)" to upload$/) do | filename |
+  page.attach_file :upload_file, File.join(Rails.root, 'spec', 'fixtures','uploaded_files', filename)
 end
 
-And(/^I should see "([^"]*)" uploaded for this membership application$/) do |arg|
-  pending
-  # is this really any different from just seeing content on the page?
-  # expect the specific <div> to have this content
-  # FIXME expect(page).to
+And(/^I should see "([^"]*)" uploaded for this membership application$/) do |filename|
+  expect(page).to have_selector('.uploaded_file', text: filename)
 end
 
-And(/^there is a file named "([^"]*)" uploaded for this membership application$/) do |arg|
+And(/^I should not see "([^"]*)" uploaded for this membership application$/) do |filename|
+  expect(page).not_to have_selector('.uploaded_file', text: filename)
+end
+
+And(/^there is a file named "([^"]*)" uploaded for this membership application$/) do |filename|
   pending
-  # FIXME how do we test for this?  (stub it out here with a fixture)
+  # FIXME how do we test for this? test for file existence give the Paperclip configuration path set for this environment
 end
 
 And(/^I should see (\d+) uploaded files listed$/) do |number|
-  expect(page).to have_selector('.uploaded_files', count: number)
+  expect(page).to have_selector('.uploaded_file', count: number)
 end
+
