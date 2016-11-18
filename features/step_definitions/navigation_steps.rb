54fe8b840039e68a8b8df2340636596574cf1c6b
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-17 23:46:33 -0800

    add valid path for a new membership application (navigation step)

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -6,6 +6,8 @@ Given(/^I am on the "([^"]*)" page$/) do |page|
       path = new_user_session_path
     when 'edit my application'
       path = edit_membership_application_path(@user.membership_applications.last)
+    when 'submit new membership application'
+      path = new_membership_application_path
     else
       path = 'no path set'
   end
