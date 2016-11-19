3338bfc4aa8dfb61e1a07530e4d7638c4eca8d74
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-18 23:23:18 -0800

    add paperclip attached file :actual_file to the UploadedFile model; add valid content types, must be present

--- app/models/uploaded_file.rb
+++ app/models/uploaded_file.rb
@@ -1,2 +1,16 @@
 class UploadedFile < ApplicationRecord
+  has_attached_file :actual_file
+
+#  validates_attachment_content_type :actual_file, content_type: {content_type: ['image/jpeg', 'image/gif', 'image/png',
+#                                                     'text/plain',
+#                                                     'text/rtf',
+#                                                     'application/pdf',
+#                                                     'application/msword'] }
+
+  validates_attachment :actual_file, presence: true,
+                       content_type: { content_type: ['image/jpeg', 'image/gif', 'image/png',
+                                                     'text/plain',
+                                                     'text/rtf',
+                                                     'application/pdf',
+                                                     'application/msword'] }
 end
