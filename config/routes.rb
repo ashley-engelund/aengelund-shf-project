d98364217fe6e3f86e9196fe83ff5705e8ac0262
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-18 21:03:36 -0800

    rails g scaffold UploadedFile title:string description:string; db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :uploaded_files
   devise_for :users
   root to: 'application#index.html'
   resources :membership_applications, only: [:new, :create, :edit, :update, :index, :show]
---
+++ spec/factories/uploaded_files.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :uploaded_file do
+    title "MyString"
+    description "MyString"
+  end
+end
