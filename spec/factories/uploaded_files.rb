add01c7fa196a08ad3b3ce8b3750ecc5e684d8bb
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-18 22:03:47 -0800

    factory for UploadedFile, with traits and factories for graphic content types

--- spec/factories/uploaded_files.rb
+++ spec/factories/uploaded_files.rb
@@ -1,6 +1,21 @@
 FactoryGirl.define do
   factory :uploaded_file do
-    title "MyString"
-    description "MyString"
+    title "some title"
+    description "some description"
+
+    trait :png do
+      actual_file { File.new("#{Rails.root}/spec/support/fixtures/image.png") }
+    end
+    trait :gif do
+      actual_file { File.new("#{Rails.root}/spec/support/fixtures/image.gif") }
+    end
+    trait :jpg do
+      actual_file { File.new("#{Rails.root}/spec/support/fixtures/image.jpg") }
+    end
+
+    factory :png_attachment,    traits: [:png]
+    factory :jpg_attachment,    traits: [:jpg]
+    factory :gif_attachment,    traits: [:gif]
   end
+
 end
