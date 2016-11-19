206d70cc753fd5d0e5ba70fbbfbcf4b9c4768ddd
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-18 23:28:29 -0800

    unit test just checks that our valid content types are ok, and that others are rejected

--- spec/models/uploaded_file_spec.rb
+++ spec/models/uploaded_file_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe UploadedFile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "accepted content types" do
+    it "png" do
+      expect(build(:uploaded_file, :png)).to be_valid
+    end
+    it "gif" do
+      expect(build(:uploaded_file, :gif)).to be_valid
+    end
+    it "jpg" do
+      expect(build(:uploaded_file, :jpg)).to be_valid
+    end
+
+    it "pdf" do
+      expect(build(:uploaded_file, :pdf)).to be_valid
+    end
+    it "txt" do
+      expect(build(:uploaded_file, :txt)).to be_valid
+    end
+
+  end
+
+  describe "unacceptable contented types" do
+
+    it "binary" do
+      expect(build(:uploaded_file, :bin)).not_to be_valid
+    end
+
+    it ".exe" do
+      expect(build(:uploaded_file, :exe)).not_to be_valid
+    end
+
+
+  end
+
 end
