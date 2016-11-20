c1cb849c97b6b4a54b887abbeef72373bcedcc2c
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-20 10:10:52 -0800

    class names should use dashes (hyphens), not underscores

--- features/step_definitions/upload_steps.rb
+++ features/step_definitions/upload_steps.rb
@@ -1,15 +1,15 @@
 
 
 When(/^I choose a file named "([^"]*)" to upload$/) do | filename |
-  page.attach_file :upload_file, File.join(Rails.root, 'spec', 'fixtures','uploaded_files', filename)
+  page.attach_file "uploaded_file[actual_file]", File.join(Rails.root, 'spec', 'fixtures','uploaded_files', filename)
 end
 
 And(/^I should see "([^"]*)" uploaded for this membership application$/) do |filename|
-  expect(page).to have_selector('.uploaded_file', text: filename)
+  expect(page).to have_selector('.uploaded-file', text: filename)
 end
 
 And(/^I should not see "([^"]*)" uploaded for this membership application$/) do |filename|
-  expect(page).not_to have_selector('.uploaded_file', text: filename)
+  expect(page).not_to have_selector('.uploaded-file', text: filename)
 end
 
 And(/^there is a file named "([^"]*)" uploaded for this membership application$/) do |filename|
@@ -18,6 +18,6 @@ And(/^there is a file named "([^"]*)" uploaded for this membership application$/
 end
 
 And(/^I should see (\d+) uploaded files listed$/) do |number|
-  expect(page).to have_selector('.uploaded_file', count: number)
+  expect(page).to have_selector('.uploaded-file', count: number)
 end
 
