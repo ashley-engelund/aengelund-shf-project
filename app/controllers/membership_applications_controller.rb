fb21af46e21684bed6e47ccaec9e48365a2ad8cb
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-20 10:18:28 -0800

    handle a new uploaded file for a membership app: must check parameters and see if a new file was uploaded

--- app/controllers/membership_applications_controller.rb
+++ app/controllers/membership_applications_controller.rb
@@ -1,14 +1,19 @@
 class MembershipApplicationsController < ApplicationController
-before_action :get_membership_application, only: [:show, :edit, :update]
-before_action :authorize_membership_application, only: [ :update, :show, :edit]
+  before_action :get_membership_application, only: [:show, :edit, :update]
+  before_action :authorize_membership_application, only: [:update, :show, :edit]
+
 
   def new
     @membership_application = MembershipApplication.new
+    @uploaded_file = @membership_application.uploaded_files.build
   end
+
 
   def create
     @membership_application = current_user.membership_applications.new(membership_application_params)
     if @membership_application.save
+      new_upload_file params['uploaded_file'] if params['uploaded_file']
+
       flash[:notice] = 'Thank you, Your application has been submitted'
       redirect_to root_path
     else
@@ -16,21 +21,27 @@ before_action :authorize_membership_application, only: [ :update, :show, :edit]
     end
   end
 
+
   def index
     authorize MembershipApplication
     @membership_applications = MembershipApplication.all
   end
 
+
   def show
 
   end
+
 
   def edit
 
   end
 
+
   def update
     if @membership_application.update(membership_application_params)
+      new_upload_file params['uploaded_file'] if params['uploaded_file']
+
       flash[:notice] = 'Membership Application
                         successfully updated'
       render :show
@@ -47,11 +58,24 @@ before_action :authorize_membership_application, only: [ :update, :show, :edit]
     params.require(:membership_application).permit(*policy(@membership_application || MembershipApplication).permitted_attributes)
   end
 
+
   def get_membership_application
     @membership_application = MembershipApplication.find(params[:id])
   end
 
+
   def authorize_membership_application
     authorize @membership_application
+  end
+
+  def new_upload_file(upload_file_param)
+    if upload_file_param['actual_file']
+      @uploaded_file = @membership_application.uploaded_files.create(actual_file: upload_file_param['actual_file'])
+      if @uploaded_file.valid?
+        flash[:notice] = "The file was uploaded: #{@uploaded_file.actual_file_file_name}"
+      else
+        flash[:error] = @uploaded_file.errors.messages
+      end
+    end
   end
 end
