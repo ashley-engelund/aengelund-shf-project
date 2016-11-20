96a4c6cf95ccd02d1655f2f3d3ba00daabc93f21
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-20 10:15:10 -0800

    create partial to show the list of uploaded files for an application; use helper method to format info for each file

--- app/helpers/membership_application_helper.rb
+++ app/helpers/membership_application_helper.rb
@@ -4,4 +4,11 @@ module MembershipApplicationHelper
     policy(@membership_application).permitted_attributes_for_edit.include? :status
   end
 
+  def uploaded_file_inline(upload_file)
+    "#{upload_file.actual_file_file_name} #{label_unless_blank('title', upload_file.title)} #{label_unless_blank('description', upload_file.description)}"
+  end
+
+  def label_unless_blank(label, value)
+    value.blank? ? '' : "#{label}: #{value}"
+  end
 end
