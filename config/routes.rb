c8455dc32015d2e106937c8b224b4145ea485d08
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-20 21:56:18 -0800

    make uploaded_files a nested resource of membership_applications

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :uploaded_files
   devise_for :users
   root to: 'application#index.html'
-  resources :membership_applications, only: [:new, :create, :edit, :update, :index, :show]
+  resources :membership_applications, only: [:new, :create, :edit, :update, :index, :show] do
+    resources :uploaded_files
+  end
 
 end
