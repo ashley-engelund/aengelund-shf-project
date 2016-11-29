10c3f61bce53b58dc212c73b3a8a7dc3f1a3743a
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-29 12:17:09 -0800

    add uploaded_files to membership application (to merge code pulled from  current development branch)

--- app/models/membership_application.rb
+++ app/models/membership_application.rb
@@ -1,6 +1,8 @@
 class MembershipApplication < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :business_categories
+  has_many :uploaded_files
+
   validates_presence_of :first_name,
                         :last_name,
                         :company_number,
@@ -8,5 +10,7 @@ class MembershipApplication < ApplicationRecord
                         :status
   validates_length_of :company_number, is: 10
   validates_format_of :contact_email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: [:create, :update]
+  accepts_nested_attributes_for :uploaded_files, allow_destroy: true,
+                                reject_if: lambda {|attributes| attributes['actual_file_file_name'].blank?}
 
 end
