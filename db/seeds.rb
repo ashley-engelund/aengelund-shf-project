11cb70c5ea63c5c0c7fd606d664cad3193e369c7
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-29 15:33:17 -0800

    business categories from PT https://www.pivotaltracker.com/story/show/133081453/comments/155585923

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(email: 'admin@sverigeshundforetagare.se', password: 'hundapor', admin: true)
-BusinessCategory.create(name:'Groomer')
-BusinessCategory.create(name: 'Trainer')
+
+
+business_categories = ['Träning', 'Psykologi', 'Rehab', 'Butik', 'Trim', 'Friskvård', 'Dagis', 'Pensionat', 'Rastare', 'Sociala tjänstehundar (vårdhund mm)', 'Civila tjänstehundar (med en förare)', 'Skola']
+business_categories.each { |b_category| BusinessCategory.create(name: b_category)}
