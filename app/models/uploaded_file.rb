8d7ec09f1639510bee2e10feb13b1a7ea7b4016c
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-29 12:55:07 -0800

    added accepted content types:  Microsoft word .doc, .docm, .docx  (and indentation formatting)

--- app/models/uploaded_file.rb
+++ app/models/uploaded_file.rb
@@ -4,10 +4,14 @@ class UploadedFile < ApplicationRecord
 
   has_attached_file :actual_file
 
-  validates_attachment :actual_file, content_type: { content_type: ['image/jpeg', 'image/gif', 'image/png',
-                                                     'text/plain',
-                                                     'text/rtf',
-                                                     'application/pdf',
-                                                     'application/msword'],
-                                       message: 'Sorry, this is not a file type you can upload.'}
+  validates_attachment :actual_file, content_type: {content_type: ['image/jpeg',
+                                                                   'image/gif',
+                                                                   'image/png',
+                                                                   'text/plain',
+                                                                   'text/rtf',
+                                                                   'application/pdf',
+                                                                   'application/msword',
+                                                                   'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                                                                   'application/vnd.ms-word.document.macroEnabled.12'],
+                                                    message: 'Sorry, this is not a file type you can upload.'}
 end
