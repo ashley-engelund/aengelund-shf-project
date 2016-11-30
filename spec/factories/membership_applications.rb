ae29fa9d9722a441789a333c36c7399ccf0bac51
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-30 01:14:48 -0800

    be able to set the business category name, the number of business categories (and the base business category name for the sequence)

--- spec/factories/membership_applications.rb
+++ spec/factories/membership_applications.rb
@@ -1,10 +1,33 @@
 FactoryGirl.define do
+
+  sequence(:cat_name_seq, "Business Category", 1) { |name, num| "#{name} #{num}" }
+
   factory :membership_application do
+
     first_name "Firstname"
     last_name "Lastname"
     company_number '1111111111'
     phone_number "MyString"
     contact_email "MyString@email.com"
     association :user
+
+
+    transient do
+      num_categories 1
+      category_name "Business Category"
+    end
+
+    after(:build) do |membership_app, evaluator|
+
+      if evaluator.num_categories == 1
+        membership_app.business_categories << build(:business_category, name: evaluator.category_name)
+      else
+        evaluator.num_categories.times do |cat_num|
+          membership_app.business_categories << build(:business_category, name: "#{evaluator.category_name} #{cat_num + 1}")
+        end
+      end
+    end
+
+
   end
 end
