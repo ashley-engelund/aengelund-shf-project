13c351f1ff2384b448d5133eb86059bac521ed6b
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-30 04:45:23 -0800

    a membership app. can have a company associated (once it's approved)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130042130) do
+ActiveRecord::Schema.define(version: 20161130122729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20161130042130) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "contact_email"
+    t.integer  "company_id"
+    t.index ["company_id"], name: "index_membership_applications_on_company_id", using: :btree
     t.index ["user_id"], name: "index_membership_applications_on_user_id", using: :btree
   end
 
