011a2065b6a0708812321febf08883f275f41824
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-30 02:37:17 -0800

    only an admin can create a new business category

--- app/policies/business_category_policy.rb
+++ app/policies/business_category_policy.rb
@@ -1,33 +1,15 @@
 class BusinessCategoryPolicy < ApplicationPolicy
 
+  def new?
+    is_admin?
+  end
+
+  def create?
+    new?
+  end
 
   def show?
     true
   end
 
-
-  def index?
-    is_admin?
-  end
-
-
-  def update?
-    is_admin?
-  end
-
-
-  def edit?
-    is_admin?
-  end
-
-
-  def destroy?
-    is_admin?
-  end
-
-
-  private
-  def is_admin?
-    @user.admin? if @user
-  end
 end
