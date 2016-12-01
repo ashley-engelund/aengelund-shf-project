b1894b369643a032e1ef24dc6478ee4a240b2714
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-30 18:31:37 -0800

    remove 'title' and 'description' from the uploaded_files table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129022533) do
+ActiveRecord::Schema.define(version: 20161201022139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,8 +43,6 @@ ActiveRecord::Schema.define(version: 20161129022533) do
   end
 
   create_table "uploaded_files", force: :cascade do |t|
-    t.string   "title"
-    t.string   "description"
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.string   "actual_file_file_name"
