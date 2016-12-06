d53e5daefa3692fb19920bfe913071d9e1280a35
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-12-06 11:14:38 -0800

    add methods has_membership_application? has_company?

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def has_membership_application?
+    membership_applications.size > 0
+  end
+
+  def has_company?
+    has_membership_application? && !membership_applications.last.company.nil?
+  end
+
 end
