d29390fe95419dd355d1137332b6f228d581494f
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-12-08 12:04:17 -0800

    user factory: create member (has approved membership), create user with a membership submitted

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,27 +7,19 @@ FactoryGirl.define do
     admin false
     is_member false
 
-    transient do
-      company_number 5712213304
-      company nil
-      first_name 'FirstName'
-      last_name 'LastName'
-      category_name 'Category1'
-      status 'pending'
-    end
 
     factory :user_with_membership_app do
       after(:create) do |user, evaluator|
-        create_list(:membership_application, 1, user: user, first_name: evaluator.first_name, category_name: evaluator.category_name, company_number: evaluator.company_number, contact_email: user.email)
-        user.save
+        create_list(:membership_application, 1, user: user, company_number: 5712213304)
       end
     end
 
     factory :member_with_membership_app do
+      is_member true
       after(:create) do |user, evaluator|
-        user.is_member = true
-        create_list(:membership_application, 1, user: user, status: 'Accepted', first_name: evaluator.first_name, category_name: evaluator.category_name, company_number: evaluator.company_number, contact_email: user.email)
-        user.save
+        evaluator.is_member = true
+        create_list(:membership_application, 1, user: user, company_number: 5562728336, status: 'Godkänd')
+
       end
     end
 
