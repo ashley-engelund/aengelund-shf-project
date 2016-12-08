482fbb8e69b40aa14fd6f24edbb10347e7afff5b
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-12-08 14:27:34 -0800

    be able to have the company number specified

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,26 +7,34 @@ FactoryGirl.define do
     admin false
     is_member false
 
+    transient do
+      company_number 5712213304
+    end
 
     factory :user_with_membership_app do
+
       after(:create) do |user, evaluator|
-        create_list(:membership_application, 1, user: user, company_number: 5712213304)
+        create_list(:membership_application, 1, user: user, contact_email: evaluator.email, company_number: evaluator.company_number)
       end
     end
 
     factory :user_with_2_membership_apps do
       after(:create) do |user, evaluator|
-        create_list(:membership_application, 2, user: user, company_number: 5712213304)
+        create_list(:membership_application, 2, user: user, contact_email: evaluator.email, company_number: evaluator.company_number)
       end
 
     end
 
     factory :member_with_membership_app do
       is_member true
-      after(:create) do |user, evaluator|
-        evaluator.is_member = true
-        create_list(:membership_application, 1, user: user, company_number: 5562728336, status: 'Godkänd')
 
+      transient do
+        company_number 5562728336
+      end
+
+      after(:create) do |user, evaluator|
+        user.is_member = true
+        create_list(:membership_application, 1, user: user, contact_email: evaluator.email , company_number:  evaluator.company_number, status: 'Godkänd')
       end
     end
   end
