87a4d5d1cecf4f8b8251953d7c2c91a929d39097
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-12-11 01:07:42 -0800

    route admin user to admin_root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: 'companies#index'
+
+  as :user do
+    authenticated :user, lambda {|u| u.admin? }  do
+      root to: "admin#index", as: :admin_root
+    end
+  end
 
   resources :business_categories
   resources :membership_applications, only: [:new, :create, :edit, :update, :index, :show]
   resources :companies, only: [:new, :create, :edit, :update, :index, :show]
+
+  root to: 'companies#index'
+
 end
