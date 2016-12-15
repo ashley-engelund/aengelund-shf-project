4f503a27005d84f390aa6db6d01642a33fc392f6
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-12-15 03:44:02 -0800

    remove unused parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
     I18n.locale = locale.to_s
   end
 
-  def default_url_options(options = {})
+  def default_url_options
     {locale: I18n.locale}
   end
 
