f39ddeeb62cb39ac32543cbf9117a3403098dfcf
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-12-17 18:57:27 -0800

    rename list_categories to reflect what it does (only returns last); add method to return a string with all category names. update spec

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -4,9 +4,18 @@ module CompaniesHelper
     !company.nil? && !company.name.nil? && !company.name.empty? &&
       !company.region.nil? && !company.region.empty?
   end
-  def list_categories company
-    if company.categories.any?
-      company.categories.last.name
+
+  def last_category_name company
+    if company.business_categories.any?
+      company.business_categories.last.name
     end
+
+  end
+
+  def list_categories company
+    if company.business_categories.any?
+      company.business_categories.order(:name).map(&:name).join(" ")
+    end
+
   end
 end
--- spec/helpers/companies_helper_spec.rb
+++ spec/helpers/companies_helper_spec.rb
@@ -7,41 +7,65 @@ RSpec.describe CompaniesHelper, type: :helper do
       company = nil
       expect(helper.company_complete?(company)).to eq false
     end
+
     it 'returns false if company name is nil' do
       company = FactoryGirl.create(:company, name: nil)
       expect(helper.company_complete?(company)).to eq false
     end
+
     it 'returns false if company region is nil' do
       company = FactoryGirl.create(:company, region: nil)
       expect(helper.company_complete?(company)).to eq false
     end
+
     it 'returns false is company name or region is empty string' do
       company = FactoryGirl.create(:company, name: '')
       expect(helper.company_complete?(company)).to eq false
-
     end
+
     it 'returns true if company name and region are non-empty string' do
       company = FactoryGirl.create(:company)
       expect(helper.company_complete?(company)).to eq true
     end
+
   end
 
-  describe '#list_companies' do
-    it 'returns a companys categories' do
-       FactoryGirl.create(:membership_application,
-                          company_number: 5562252998,
-                          category_name: 'Träning')
-      company = FactoryGirl.create(:company,
-                                    company_number: 5562252998)
-      expect(helper.list_categories(company)).to eq 'Träning'
+  describe 'companies' do
+
+    it '#last_category_name' do
+      Company.delete_all
+      MembershipApplication.delete_all
+      User.delete_all
+      company = FactoryGirl.create(:company, company_number: 5562252998)
+
+      employee1 = create(:user, email: 'emp1@happymutts.com')
+      employee2 = create(:user, email: 'emp2@happymutts.com')
+      employee3 = create(:user, email: 'emp3@happymutts.com')
+
+      create(:membership_application, user: employee1, status: 'Godkänd', num_categories: 1, category_name: 'cat1', company_number: '5562252998')
+      create(:membership_application, user: employee2, status: 'Godkänd', num_categories: 1, category_name: 'cat2', company_number: '5562252998')
+      create(:membership_application, user: employee3, status: 'Godkänd', num_categories: 1, category_name: 'cat3', company_number: '5562252998')
+
+      expect(helper.last_category_name(company)).to eq 'cat3'
+
     end
-    it 'returns correct categories' do
-      FactoryGirl.create(:membership_application,
-                         company_number: 5562252998,
-                         category_name: 'Whatever')
-      company = FactoryGirl.create(:company,
-                                   company_number: 5562252998)
-      expect(helper.list_categories(company)).not_to eq 'Träning'
+
+    it '#list_categories' do
+      Company.delete_all
+      MembershipApplication.delete_all
+      User.delete_all
+      company = FactoryGirl.create(:company, company_number: 5562252998)
+
+      employee1 = create(:user, email: 'emp1@happymutts.com')
+      employee2 = create(:user, email: 'emp2@happymutts.com')
+      employee3 = create(:user, email: 'emp3@happymutts.com')
+
+      create(:membership_application, user: employee1, status: 'Godkänd', num_categories: 1, category_name: 'cat1', company_number: '5562252998')
+      create(:membership_application, user: employee2, status: 'Godkänd', num_categories: 1, category_name: 'cat2', company_number: '5562252998')
+      create(:membership_application, user: employee3, status: 'Godkänd', num_categories: 1, category_name: 'cat3', company_number: '5562252998')
+
+      expect(helper.list_categories(company)).to eq 'cat1 cat2 cat3'
+      expect(helper.list_categories(company)).not_to include 'Träning'
     end
   end
 end
