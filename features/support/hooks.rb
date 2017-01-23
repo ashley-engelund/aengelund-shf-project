46b352b5dff5aba8fd569a8711f0e50594694fe5
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2017-01-23 01:54:47 -0800

    create a @poltergeist tag to invoke the poltergeist driver when javascript isn't explicitly being used

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -6,3 +6,13 @@ After('@javascript') do
   Capybara.reset_sessions!
   Capybara.current_driver = :rack_test
 end
+
+
+Before('@poltergeist') do
+  Capybara.current_driver = :poltergeist
+end
+
+After('@poltergeist') do
+  Capybara.reset_sessions!
+  Capybara.current_driver = :rack_test
+end
