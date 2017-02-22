038ce554adeb0a0b5137cd1c87c07101d50c7943
Ashley Engelund <ashleyengelund@Ashleys-MBP.home>
2017-02-21 18:15:18 -0800

    controller: add before_actions to set a specific @user and to authorize the current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
+  before_action :authorize_user
+  before_action :set_user, only: :show
+
+
   def index
     authorize User
 
     @users = User.all.order(last_sign_in_at: :asc)
   end
+
+
+
+  private
+
+  def authorize_user
+    authorize User
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
