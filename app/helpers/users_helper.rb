0ee973379fd897dfc1b731ff3000384a8ce584f2
Ashley Engelund <ashleyengelund@Ashleys-MBP.home>
2017-02-22 17:16:10 -0800

    remove helper for showing IP address from most recent login (not used)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,8 +4,5 @@ module UsersHelper
     user.current_sign_in_at.blank? ? user.last_sign_in_at : user.current_sign_in_at
   end
 
-  def most_recent_login_ip user
-    user.current_sign_in_ip.blank? ? user.last_sign_in_ip : user.current_sign_in_ip
-  end
 
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -32,32 +32,4 @@ RSpec.describe UsersHelper, type: :helper do
   end
 
 
-  describe '#most_recent_login_ip' do
-
-    it 'returns nil if the user has never logged in' do
-      expect(helper.most_recent_login_ip(user)).to be_nil
-    end
-
-    it 'returns the current_sign_in_ip if current_sign_in_at is not nil' do
-      u = user
-      u.update(current_sign_in_ip: '8.8.8.8')
-      expect(helper.most_recent_login_ip(u)).to eq('8.8.8.8')
-    end
-
-    it 'returns the current_sign_in_ip if it is not nil and there was a last_sign_in_ip' do
-      u = user
-      u.update(current_sign_in_ip: '8.8.8.8', last_sign_in_ip: '127.0.0.1')
-      expect(helper.most_recent_login_ip(u)).to eq('8.8.8.8')
-    end
-
-
-    it 'returns the last_sign_in_ip if current_sign_in_ip is nil' do
-      u = user
-      u.update( last_sign_in_ip: '127.0.0.1')
-      expect(helper.most_recent_login_ip(u)).to eq('127.0.0.1')
-    end
-
-  end
-
-
 end
