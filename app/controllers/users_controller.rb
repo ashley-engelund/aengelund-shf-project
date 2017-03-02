17a3eb427cd4ee290b2d164c4d272008ced566cc
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-03-02 11:02:36 -0800

    check that the password entered matches the 2nd entry to confirm it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authorize_user
-  before_action :set_user, only: :show
+  before_action :set_user, only: [:show, :update]
 
 
   def index
@@ -8,6 +8,16 @@ class UsersController < ApplicationController
   end
 
 
+  def update
+
+    if passwords_match(user_params) && @user.update(user_params)
+      redirect_to @user, notice: t('.success')
+    else
+      flash[:alert] = t('.error')
+      render :show
+    end
+
+  end
 
   private
 
@@ -19,4 +29,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+
+  # if the password was changed, ensure the confirmation matches the 1st one entered
+  def passwords_match(u_params)
+    u_params.key?('password') && u_params.key?('password_confirmation') && u_params['password'] == u_params['password_confirmation']
+  end
 end
