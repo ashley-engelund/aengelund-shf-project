5954ef2afc6f78baf77f5fa79688a1f5c75dd3c8
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-03-05 13:46:30 -0800

    add action to update the user info; test to see if 2 passwords match

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,23 @@
 class UsersController < ApplicationController
   before_action :authorize_user
-  before_action :set_user, only: :show
+  before_action :set_user, except: :index
 
 
   def index
     @users = User.all.order(last_sign_in_at: :asc)
   end
 
+
+  def update
+
+    if passwords_match(user_params) && @user.update(user_params)
+      redirect_to @user, notice: t('.success')
+    else
+      flash[:alert] = t('.error') + '  ' + t('.passwords_dont_match')
+      render :show
+    end
+
+  end
 
 
   private
@@ -15,8 +26,22 @@ class UsersController < ApplicationController
     authorize User
   end
 
+
   def set_user
     @user = User.find(params[:id])
   end
+
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+
+  # if the password was changed, ensure the confirmation matches the 1st one entered
+  def passwords_match(u_params)
+    u_params.key?('password') && u_params.key?('password_confirmation') && u_params['password'] == u_params['password_confirmation']
+  end
+
 
 end
