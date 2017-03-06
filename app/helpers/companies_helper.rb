59daaaa93d01d3223ccf0445a543ca7879c62ad7
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-03-06 01:11:13 -0800

    text to display in a marker pop-up for a company; list categories with a given separator

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -5,9 +5,24 @@ module CompaniesHelper
   end
 
 
-  def list_categories company
+  def list_categories company, separator=' '
     if company.business_categories.any?
-      company.business_categories.order(:name).map(&:name).join(" ")
+      company.business_categories.order(:name).map(&:name).join(separator)
     end
+  end
+
+  # html to display for a company when showing a marker on a map
+  def marker_text company
+    text = "<div id='company-map-marker'>"
+      text << "<p class='name'>#{company.name}</p>"
+      text << "<p class='categories'>#{list_categories company, ', '}</p>"
+      text << "<br>"
+    company.addresses.each do | addr |
+      text << "<p class='entire-address'>#{addr.entire_address}</p>"
+    end
+
+    text << "</div>"
+
+    text
   end
 end
