2a707488b33f0a9cf73a5901265f9c5f7f08fca2
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-03-25 17:41:44 -0700

    ShfDocumentPolicy and spec

---
+++ app/policies/shf_document_policy.rb
@@ -0,0 +1,28 @@
+class ShfDocumentPolicy < ApplicationPolicy
+
+
+  def index?
+    @user.is_member_or_admin? if @user
+  end
+
+  def update?
+    is_admin?
+  end
+
+
+  def show?
+    is_admin?
+  end
+
+
+  def new?
+    create?
+  end
+
+
+  def create?
+    is_admin?
+  end
+
+
+end
