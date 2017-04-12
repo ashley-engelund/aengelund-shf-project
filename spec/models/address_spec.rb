52070118ccb626b5bcf9df07476e31733543c7eb
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-04-12 13:25:22 -0700

    Address spec: use less precision when checking the longitude

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -107,7 +107,7 @@ RSpec.describe Address, type: :model do
       addr.validate
 
       expect(addr.latitude.round(2)).to eq(56.7440333.round(2)), addr_details(addr, "expected lat to be 56.744, but wasn't")
-      expect(addr.longitude.round(2)).to eq(12.727637.round(2)), addr_details(addr, "expected long to be 12.728, but wasn't")
+      expect(addr.longitude.round(2)).to eq(12.727637.round(0)), addr_details(addr, "expected long to be 12.728, but wasn't")
     end
 
 
@@ -128,7 +128,7 @@ RSpec.describe Address, type: :model do
         expect(addr.longitude).not_to eq(orig_long)
 
         expect(addr.latitude.round(2)).to eq(56.7442343.round(2)), addr_details(addr, "expected lat to be 56.744, but wasn't")
-        expect(addr.longitude.round(2)).to eq(12.7255982.round(2)), addr_details(addr, "expected long to be 12.726, but wasn't")
+        expect(addr.longitude.round(2)).to eq(12.7255982.round(0)), addr_details(addr, "expected long to be 12.726, but wasn't")
       end
 
       it 'changed kommun' do
