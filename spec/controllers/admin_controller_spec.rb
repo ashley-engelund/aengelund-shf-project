09ed685322748ec1de6d0e188556b36f5db9d864
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-05-09 00:29:14 -0700

    refactor the export method(s) since the functionality and complexity has increased

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -13,22 +13,32 @@ RSpec.describe AdminController, type: :controller do
   include Devise::Test::ControllerHelpers
 
   # this will bypass Pundit policy access checks so logging in is not necessary
-  before(:each) { Warden.test_mode! }
+  before(:each) {Warden.test_mode!}
 
-  after(:each) { Warden.test_reset! }
+  after(:each) {Warden.test_reset!}
 
-  let(:user) { create(:user) }
+  let(:user) {create(:user)}
 
-  let(:csv_header) { out_str = ''
+  let(:csv_header) {out_str = ''
   out_str << "'#{I18n.t('activerecord.attributes.membership_application.contact_email').strip}',"
   out_str << "'#{I18n.t('activerecord.attributes.membership_application.first_name').strip}',"
   out_str << "'#{I18n.t('activerecord.attributes.membership_application.last_name').strip}',"
   out_str << "'#{I18n.t('activerecord.attributes.membership_application.membership_number').strip}',"
-  out_str << "'#{I18n.t('activerecord.attributes.membership_application.state').strip}'\n"
-  out_str }
+  out_str << "'#{I18n.t('activerecord.attributes.membership_application.state').strip}',"
+  out_str << "'#{I18n.t('activerecord.models.company.one').strip}',"
+  out_str << "'#{I18n.t('activerecord.attributes.address.street').strip}',"
+  out_str << "'#{I18n.t('activerecord.attributes.address.post_code').strip}',"
+  out_str << "'#{I18n.t('activerecord.attributes.address.city').strip}',"
+  out_str << "'#{I18n.t('activerecord.attributes.address.kommun').strip}',"
+  out_str << "'#{I18n.t('activerecord.attributes.address.region').strip}',"
+  out_str << "'#{I18n.t('activerecord.attributes.address.country').strip}'"
+  out_str << "\n"
+  out_str}
+
 
 
   describe '#export_ankosan_csv' do
+
 
     describe 'logged in as admin' do
 
@@ -39,6 +49,16 @@ RSpec.describe AdminController, type: :controller do
         expect(response.content_type).to eq 'text/plain'
 
       end
+
+
+      it 'header line is correct' do
+
+        post :export_ansokan_csv
+
+        expect(response.body).to eq  csv_header
+
+      end
+
 
 
       describe 'with 0 membership applications' do
@@ -53,11 +73,12 @@ RSpec.describe AdminController, type: :controller do
 
       end
 
-      describe 'with 1 app for each state' do
+
+      describe 'with 1 app for each membership state' do
 
         it 'includes all applications' do
 
-          result_str = ''
+          result_str = csv_header
 
           # create 1 application in each state
           MembershipApplication.aasm.states.each do |app_state|
@@ -65,12 +86,21 @@ RSpec.describe AdminController, type: :controller do
             u = FactoryGirl.create(:user, email: "#{app_state.name.to_s}@example.com")
 
             m = FactoryGirl.create :membership_application,
-                                   first_name: "First#{app_state.name.to_s}",
-                                   last_name: "Last#{app_state.name.to_s}",
+                                   first_name:    "First#{app_state.name.to_s}",
+                                   last_name:     "Last#{app_state.name.to_s}",
                                    contact_email: "#{app_state.name.to_s}@example.com",
-                                   state: app_state.name,
-                                   user: u
-            result_str << "#{m.contact_email},#{m.first_name},#{m.last_name},#{m.membership_number},"+ I18n.t("membership_applications.state.#{app_state.name}") +"\n"
+                                   state:         app_state.name,
+                                   user:          u
+
+            member1_info = "#{m.contact_email},#{m.first_name},#{m.last_name},#{m.membership_number},"+ I18n.t("membership_applications.state.#{app_state.name}")
+
+            result_str << member1_info + ','
+
+            result_str << (m.company.nil? ? '' : '"' + m.company.name + '"' )
+            result_str <<  ','
+
+            result_str << m.se_mailing_csv_str + "\n"
+
           end
 
           post :export_ansokan_csv
@@ -79,16 +109,55 @@ RSpec.describe AdminController, type: :controller do
 
         end
 
+      end
+
+
+      describe 'includes mailing addresses' do
+
+        def address_string(address)
+          "#{address.street_address},#{address.post_code},#{address.city},#{address.kommun.name },#{address.region.name},#{address.country_postal}"
+        end
+
+
+        it 'uses the company name and  address for each member' do
+
+          u1      = FactoryGirl.create(:user, email: "user1@example.com")
+          c1      = FactoryGirl.create(:company)
+          member1 = FactoryGirl.create :membership_application,
+                                       first_name:     "u1",
+                                       contact_email:  "u1@example.com",
+                                       state:          :accepted,
+                                       company_number: c1.company_number,
+                                       user:           u1
+
+          result_str = csv_header
+
+          member1.update(membership_number: '1234567890')
+
+          member1_info              = "#{member1.contact_email},#{member1.first_name},#{member1.last_name},#{member1.membership_number},"+ I18n.t("membership_applications.state.#{member1.state}")
+
+          result_str << member1_info + ','
+          result_str << '"' + c1.name + '"' +','
+
+          result_str << c1.se_mailing_csv_str + "\n"
+
+
+          post :export_ansokan_csv
+
+          expect(response.body).to match result_str
+
+        end
 
       end
+
 
       describe 'error from send_data is rescued' do
 
         # status, location, response_body
 
-        let(:error_message) {'Error. Error. Warning Will Robinson' }
+        let(:error_message) {'Error. Error. Warning Will Robinson'}
 
-        subject { allow(@controller).to receive(:send_data) { raise StandardError.new(error_message) }
+        subject {allow(@controller).to receive(:send_data) {raise StandardError.new(error_message)}
 
         post :export_ansokan_csv
         }
