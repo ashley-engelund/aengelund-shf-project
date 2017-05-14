5808ce1c89388f3fd3cc43854fc41079796b1113
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-05-14 13:47:12 -0700

    get reasons for waiting from locale file (instead of db)

--- app/helpers/membership_applications_helper.rb
+++ app/helpers/membership_applications_helper.rb
@@ -8,4 +8,14 @@ module MembershipApplicationsHelper
     @membership_application ? "#{@membership_application.first_name} #{@membership_application.last_name}" : '..'
 
   end
+
+
+  # For now, we are assuming this list won't change a whole lot or be large.
+  #  So we'll just get the few itmes from the locale file. If this assumption
+  #  proves to be wrong, the data for the list can be moved to the db (and then get the actual translations
+  # either from the locale files or store the translations in the db somehow.)
+  def reasons_for_waiting
+      t('membership_applications.need_info.reason').invert.to_a
+  end
+  
 end
