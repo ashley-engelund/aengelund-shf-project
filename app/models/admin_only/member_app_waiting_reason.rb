97296f05325cdea2bafdd9e23176f63859398f6d
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-05-26 10:52:18 -0700

    create (scaffold+) MemberAppWaiting Reasons within the 'AdminOnly' namespace / module

---
+++ app/models/admin_only/member_app_waiting_reason.rb
@@ -0,0 +1,24 @@
+module AdminOnly
+  
+  class MemberAppWaitingReason < ApplicationRecord
+
+
+    def other_reason_placeholder?
+      name_sv == self.class.other_reason_name
+    end
+
+
+    # This is effectively a CONSTANT that is used in the UI
+    def self.other_reason_name
+      I18n.t('admin_only.member_app_waiting_reasons.other_custom_reason')
+    end
+
+
+    def self.default_name_method
+      :name_sv
+    end
+
+  end
+
+end
+
