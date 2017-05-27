0f1c7149c34dd04ab8ca402c67a858b8a094af43
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-05-27 16:08:39 -0700

    just test admin, member, user roles.

--- spec/policies/admin_policy_spec.rb
+++ spec/policies/admin_policy_spec.rb
@@ -13,15 +13,8 @@ RSpec.describe AdminPolicy do
 
   end
 
-  describe 'For a member that is a part of a company' do
-    let(:members_company) { Company.find_by_company_number('5562728336')}
-    subject { described_class.new(members_company).authorized? }
 
-    it { is_expected.to be_falsey }
-
-  end
-
-  describe 'For a member that is not part of a company' do
+  describe 'For a member' do
     subject { described_class.new(member).authorized? }
 
     it { is_expected.to be_falsey }
