b575d1e8e2ca3c494ca37e5d0b6a27562a5393c4
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-07-24 04:20:00 -0700

    +steps: should/not see images with I18n.t alt text

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -25,6 +25,16 @@ And(/^I should see "([^"]*)" image$/) do |alt_text|
   expect(page).to have_xpath("//img[contains(@alt,'#{alt_text}')]")
 end
 
+
+And(/^I should not see t\("([^"]*)"\) image$/) do |alt_text|
+  expect(page).not_to have_xpath("//img[contains(@alt,'#{i18n_content(alt_text)}')]")
+end
+
+And(/^I should see t\("([^"]*)"\) image$/) do |alt_text|
+  expect(page).to have_xpath("//img[contains(@alt,'#{i18n_content(alt_text)}')]")
+end
+
+
 And(/^I should see t\("([^"]*)"\)$/) do |content|
   expect(page).to have_content i18n_content(content)
 end
