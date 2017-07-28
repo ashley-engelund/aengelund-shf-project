80c78208118c774ebbe2e3c033475b6ee661f641
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-07-28 15:20:45 -0700

    ShfMailer with helper methods and default Mailgun domain

--- app/mailers/shf_mailer.rb
+++ app/mailers/shf_mailer.rb
@@ -1,2 +1,27 @@
 class ShfMailer < ApplicationMailer
+
+  #   @see https://github.com/mailgun/mailgun-ruby/docs for examples
+
+  # Helper methods for creating and working with Mailgun messages and API:
+
+  DEFAULT_MAILGUN_DOMAIN = 'sverigeshundforetagare.se'
+
+
+  def mailgun_client
+    Mailgun::Client.new(ENV['MAILGUN_API_KEY'])
+  end
+
+
+  def message_builder
+    mb = Mailgun::MessageBuilder.new
+    mb.from ENV['SHF_SENDER_EMAIL'] # default 'from: ' email (can overwrite as needed)
+    mb
+  end
+
+
+  # the specific SHF domain for the Mailgun account (it's not the sandbox domain)
+  def domain
+    ENV.fetch('MAILGUN_DOMAIN', DEFAULT_MAILGUN_DOMAIN)
+  end
+
 end
