29d7f31ecbbce59dbc7b7a01f58f8bfb39c63e30
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-08-07 13:13:23 -0700

    added over-clarification that the preview is not a spec/test (is located there by convention)

--- spec/mailers/previews/shf_mailer_preview.rb
+++ spec/mailers/previews/shf_mailer_preview.rb
@@ -1,5 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/shf_mailer
 
+# This is not a test or specification.  This will be run when the system is running
+# and the above url is used. @see http://guides.rubyonrails.org/action_mailer_basics.html#previewing-emails
+
 class ShfMailerPreview < ActionMailer::Preview
 
 
