ab5f86314a1a4ab4c75685b44ad14a2b909a9af4
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-09-12 18:36:52 -0700

    update the workflow for how we *really* use PivotalTracker, including when we press the buttons for a task

--- docs/dev-workflow/pivotal_tracker_feature.rb
+++ docs/dev-workflow/pivotal_tracker_feature.rb
@@ -1,77 +1,91 @@
 require 'aasm'
 
+# This is the state machine that represents how we use PivotalTracker.
+# You can use the 'aasm_statecharts' gem to create a diagram from it.
+
+# The weedySeaDragon's (Ashley Engelund's) version of 'aasm_statecharts'
+# was used to generate the .png diagram for this.  Use the following command:
+#
+#   bundle exec aasm_statecharts --include ./docs/dev-workflow pivotal_tracker_feature --table --directory ./docs/dev-workflow --config ./docs/dev-workflow/aasm-diagram-blue-green.yml
+#
+# where:
+#  '--include ./app/models'  means "include path" ./app/models
+#  'pivotal_tracker_feature'
+#  '--table'  means "include a table in the diagraph generated"
+#  '--directory ./docs/dev-workflow'  means "output to the directory " ./docs/dev-workflow
+#  '--config ./docs/dev-workflow/aasm-diagram-blue-green.yml' means use the configuration file ./docs/dev-workflow/aasm-diagram-blue-green.yml
+#
+# The weedySeaDragon version of `aasm_statecharts` is in the Gemfile.
+#
+
+
 class PivotalTrackerFeature
 
   include AASM
 
   aasm do
 
-    state :new_in_icebox, initial: true
+    state :picked_upcoming_release_from_icebox, initial: true
+
+    state :discussed
 
     state :points_assigned
 
     state :started, enter: :write_feature_or_spec, exit: :all_tests_pass
     
-    state :FINISHED__waiting_for_scrum_review, enter: :press_FINISHED_button
+    state :finished_waiting_for_scrum_review, exit: :explained_and_demoed_in_scrum_meeting
 
-    state :DELIVERED__waiting_for_client_review,  enter: :press_DELIVER_button
+    state :deployed_to_heroku_and_waiting_for_client_review, enter: :press_Finish_button_in_meeting
 
-    state :DELIVERED__waiting_for_shf_review,  enter: :press_DELIVER_button
+    state :client_accepted, enter: :press_Accepted_button_in_meeting
 
-    state :accepted, enter: :press_Accepted_button
+    state :client_rejected, enter: :press_Rejected_button_in_meeting
 
-    state :rejected, enter: :press_Rejected_button
-
-    state :deployed, enter: :deployed_to_PRODUCTION, final: true
+    state :deployed_to_production, enter: :press_Delivered_button, final: true
 
 
+    event :discuss_task do
+      transitions from: :picked_upcoming_release_from_icebox, to: :discussed
+      transitions from: :client_rejected, to: :discussed
+    end
 
     event :vote_on_feature do
-      transitions from: :new_in_icebox, to: :points_assigned, guard: :at_least_3_people_voted
+      transitions from: :discussed, to: :points_assigned, guard: :at_least_3_people_voted
     end
 
     event :start_work do
-      transitions from: :points_assigned, to: :started
-
+      transitions from: :points_assigned, to: :started, guard: :is_client_facing
+      transitions from: :discussed, to: :started, guard: :is_not_client_facing
     end
 
     event :finished_PR do
-      transitions from: :started, to: :FINISHED__waiting_for_scrum_review
-    end
-
-    event :approved_in_scrum do
-      transitions from: :FINISHED__waiting_for_scrum_review, to: :DELIVERED__waiting_for_client_review, guard: :is_client_facing
-      transitions from: :FINISHED__waiting_for_scrum_review, to: :DELIVERED__waiting_for_shf_review, guard: :is_not_client_facing
-    end
-
-    event :rejected_in_scrum do
-      transitions from: :FINISHED__waiting_for_scrum_review, to: :started
+      transitions from: :started, to: :finished_waiting_for_scrum_review
     end
 
 
-    event :deliver_to_and_review_with_client do
-
+    event :approved_in_scrum_meeting do
+      transitions from: :finished_waiting_for_scrum_review, to: :deployed_to_heroku_and_waiting_for_client_review, guard: :is_client_facing
+      transitions from: :finished_waiting_for_scrum_review, to: :deployed_to_production, guard: :is_not_client_facing
     end
 
-    event :client_accepted do
-      transitions from: :DELIVERED__waiting_for_client_review, to: :accepted, guard: :test_on_DEPLOYMENT_server_passes
+    event :shf_team_says_not_finished do
+      transitions from: :finished_waiting_for_scrum_review, to: :started
     end
 
-    event :client_rejected do
-      transitions from: :DELIVERED__waiting_for_client_review, to: :rejected
+
+    event :client_accepts do
+      transitions from: :deployed_to_heroku_and_waiting_for_client_review, to: :client_accepted, guard: :demo_on_heroku_passes
     end
 
-    event :shf_accepted do
-      transitions from: :DELIVERED__waiting_for_shf_review, to: :accepted, guard: :test_on_DEPLOYMENT_server_passes
+    event :client_rejects do
+      transitions from: :deployed_to_heroku_and_waiting_for_client_review, to: :client_rejected
     end
 
-    event :shf_rejected do
-      transitions from: :DELIVERED__waiting_for_shf_review, to: :rejected
-    end
+
 
 
     event :deploy do
-      transitions from: :accepted, to: :deployed
+      transitions from: :client_accepted, to: :deployed_to_production
     end
 
   end
