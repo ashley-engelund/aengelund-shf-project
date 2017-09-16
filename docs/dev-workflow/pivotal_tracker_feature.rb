e828d8f92c591e66c4dffd56309e3f3c3bb2944c
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-09-16 12:32:34 -0700

    PT workflow: add state for deploy_to_heroku if not client facing, other edits

--- docs/dev-workflow/pivotal_tracker_feature.rb
+++ docs/dev-workflow/pivotal_tracker_feature.rb
@@ -5,15 +5,18 @@ require 'aasm'
 
 # The weedySeaDragon's (Ashley Engelund's) version of 'aasm_statecharts'
 # was used to generate the .png diagram for this.  Use the following command:
+#  (Note that you should be in the Rails.root directory (e.g the directory
+#   above /app.)
 #
-#   bundle exec aasm_statecharts --include ./docs/dev-workflow pivotal_tracker_feature --table --directory ./docs/dev-workflow --config ./docs/dev-workflow/aasm-diagram-blue-green.yml
+#   bundle exec aasm_statecharts --include ./docs/dev-workflow pivotal_tracker_feature --directory ./docs/dev-workflow --config ./docs/dev-workflow/aasm-diagram-blue-green.yml --table
 #
 # where:
 #  '--include ./app/models'  means "include path" ./app/models
-#  'pivotal_tracker_feature'
-#  '--table'  means "include a table in the diagraph generated"
+#  'pivotal_tracker_feature' is the model to be diagrammed (it's actually the name of the .rb file for the model)
 #  '--directory ./docs/dev-workflow'  means "output to the directory " ./docs/dev-workflow
-#  '--config ./docs/dev-workflow/aasm-diagram-blue-green.yml' means use the configuration file ./docs/dev-workflow/aasm-diagram-blue-green.yml
+#  '--config aasm-diagram-blue-green.yml' means use the configuration file ./docs/dev-workflow/aasm-diagram-blue-green.yml
+#  '--table'  means "include a table in the diagraph generated"
+#
 #
 # The weedySeaDragon version of `aasm_statecharts` is in the Gemfile.
 #
@@ -25,7 +28,8 @@ class PivotalTrackerFeature
 
   aasm do
 
-    state :picked_upcoming_release_from_icebox, initial: true
+    state :picked_a_story_from_an_upcoming_release, enter: :start_work_on_story,
+          initial: true
 
     state :discussed
 
@@ -33,19 +37,24 @@ class PivotalTrackerFeature
 
     state :started, enter: :write_feature_or_spec, exit: :all_tests_pass
     
-    state :finished_waiting_for_scrum_review, exit: :explained_and_demoed_in_scrum_meeting
+    state :finished_scrum_review, enter: :explained_and_demoed_in_scrum_meeting,
+          exit: :team_approves
 
-    state :deployed_to_heroku_and_waiting_for_client_review, enter: :press_Finish_button_in_meeting
+    state :deployed_to_heroku_and_waiting_for_client_review,
+          enter: [:click_finish_and_deliver_buttons_in_meeting, :development_branch_changes_merged_to_heroku]
+
+    state :deployed_to_heroku, enter: :development_branch_changes_merged_to_heroku
 
     state :client_accepted, enter: :press_Accepted_button_in_meeting
 
     state :client_rejected, enter: :press_Rejected_button_in_meeting
 
-    state :deployed_to_production, enter: :press_Delivered_button, final: true
+    state :deployed_to_production, enter: :merge_changes_from_Heroku_to_DigitalOcean,
+          final: true
 
 
     event :discuss_task do
-      transitions from: :picked_upcoming_release_from_icebox, to: :discussed
+      transitions from: :picked_a_story_from_an_upcoming_release, to: :discussed
       transitions from: :client_rejected, to: :discussed
     end
 
@@ -59,17 +68,17 @@ class PivotalTrackerFeature
     end
 
     event :finished_PR do
-      transitions from: :started, to: :finished_waiting_for_scrum_review
+      transitions from: :started, to: :finished_scrum_review
     end
 
 
     event :approved_in_scrum_meeting do
-      transitions from: :finished_waiting_for_scrum_review, to: :deployed_to_heroku_and_waiting_for_client_review, guard: :is_client_facing
-      transitions from: :finished_waiting_for_scrum_review, to: :deployed_to_production, guard: :is_not_client_facing
+      transitions from: :finished_scrum_review, to: :deployed_to_heroku_and_waiting_for_client_review, guard: :is_client_facing
+      transitions from: :finished_scrum_review, to: :deployed_to_heroku, guard: :is_not_client_facing
     end
 
     event :shf_team_says_not_finished do
-      transitions from: :finished_waiting_for_scrum_review, to: :started
+      transitions from: :finished_scrum_review, to: :started
     end
 
 
@@ -86,6 +95,7 @@ class PivotalTrackerFeature
 
     event :deploy do
       transitions from: :client_accepted, to: :deployed_to_production
+      transitions from: :deployed_to_heroku, to: :deployed_to_production
     end
 
   end
