ad5e00ab9e5d032ab631a5bddb7bebb1c0b0dfc1
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-09-18 16:05:49 -0700

    rename class so it better reflects the intention

--- docs/dev-workflow/workflow_for_pivotal_tracker_stories.rb
+++ docs/dev-workflow/workflow_for_pivotal_tracker_stories.rb
@@ -8,7 +8,7 @@ require 'aasm'
 #  (Note that you should be in the Rails.root directory (e.g the directory
 #   above /app.)
 #
-#   bundle exec aasm_statecharts --include ./docs/dev-workflow pivotal_tracker_feature --directory ./docs/dev-workflow --config ./docs/dev-workflow/aasm-diagram-blue-green.yml --table
+#   bundle exec aasm_statecharts --include ./docs/dev-workflow workflow_for_pivotal_tracker_stories --directory ./docs/wiki --config ./docs/dev-workflow/aasm-diagram-blue-green.yml --table
 #
 # where:
 #  '--include ./app/models'  means "include path" ./app/models
@@ -20,50 +20,53 @@ require 'aasm'
 #
 # The weedySeaDragon version of `aasm_statecharts` is in the Gemfile.
 #
+# Using dot:  to create a .png file from a .dot file:
+#  dot  -Tpng -O input_dot_file.dot
+#
 
 
-class PivotalTrackerFeature
+class WorkflowForPivotalTrackerStories
 
   include AASM
 
   aasm do
 
-    state :picked_a_story_from_an_upcoming_release, enter: :start_work_on_story,
+    state :picked_a_story_from_backlog, enter: :start_work_on_story,
           initial: true
 
     state :discussed
 
     state :points_assigned
 
-    state :started, enter: :write_feature_or_spec, exit: :all_tests_pass
+    state :started, enter: [:press_PT_Start_button, :write_feature_or_spec], exit: :all_tests_pass
     
-    state :finished_scrum_review, enter: :explained_and_demoed_in_scrum_meeting,
-          exit: :team_approves
+    state :finished_scrum_review, enter: :explained_and_demoed_in_scrum_meeting
 
-    state :deployed_to_heroku_and_waiting_for_client_review,
-          enter: [:click_finish_and_deliver_buttons_in_meeting, :development_branch_changes_merged_to_heroku]
 
-    state :deployed_to_heroku, enter: :development_branch_changes_merged_to_heroku
+    state :deployed_to_Heroku, enter: [:press_PT_Finished_button_in_meeting, :development_branch_changes_merged_to_Heroku]
 
-    state :client_accepted, enter: :press_Accepted_button_in_meeting
+    state :waiting_for_client_review
 
-    state :client_rejected, enter: :press_Rejected_button_in_meeting
+    state :client_accepted, enter: :press_PT_Accepted_button_in_meeting
+
+    state :client_rejected, enter: :press_PT_Rejected_button_in_meeting
 
     state :deployed_to_production, enter: :merge_changes_from_Heroku_to_DigitalOcean,
           final: true
 
 
+
     event :discuss_task do
-      transitions from: :picked_a_story_from_an_upcoming_release, to: :discussed
+      transitions from: :picked_a_story_from_backlog, to: :discussed
       transitions from: :client_rejected, to: :discussed
     end
 
     event :vote_on_feature do
-      transitions from: :discussed, to: :points_assigned, guard: :at_least_3_people_voted
+      transitions from: :discussed, to: :points_assigned, guard: [:is_client_facing, :at_least_3_people_vote]
     end
 
     event :start_work do
-      transitions from: :points_assigned, to: :started, guard: :is_client_facing
+      transitions from: :points_assigned, to: :started
       transitions from: :discussed, to: :started, guard: :is_not_client_facing
     end
 
@@ -73,29 +76,36 @@ class PivotalTrackerFeature
 
 
     event :approved_in_scrum_meeting do
-      transitions from: :finished_scrum_review, to: :deployed_to_heroku_and_waiting_for_client_review, guard: :is_client_facing
-      transitions from: :finished_scrum_review, to: :deployed_to_heroku, guard: :is_not_client_facing
+   #   transitions from: :finished_scrum_review, to: :waiting_for_client_review, guard: :is_client_facing
+      transitions from: :finished_scrum_review, to: :deployed_to_Heroku #, guard: :is_not_client_facing
     end
 
-    event :shf_team_says_not_finished do
+
+    event :SHF_team_says_not_finished do
       transitions from: :finished_scrum_review, to: :started
     end
 
 
+    event :waiting_for_client_review_day do
+      transitions from: :deployed_to_Heroku, to: :waiting_for_client_review, guard: :is_client_facing
+    end
+
+
     event :client_accepts do
-      transitions from: :deployed_to_heroku_and_waiting_for_client_review, to: :client_accepted, guard: :demo_on_heroku_passes
+      transitions from: :waiting_for_client_review, to: :client_accepted
     end
 
     event :client_rejects do
-      transitions from: :deployed_to_heroku_and_waiting_for_client_review, to: :client_rejected
+      transitions from: :waiting_for_client_review, to: :client_rejected
     end
+
 
 
 
 
     event :deploy do
       transitions from: :client_accepted, to: :deployed_to_production
-      transitions from: :deployed_to_heroku, to: :deployed_to_production
+      transitions from: :deployed_to_Heroku, to: :deployed_to_production, guard: :is_not_client_facing
     end
 
   end
