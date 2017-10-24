49235862a41c336bbe10eab297b377806f07f416
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-10-23 17:22:31 -0700

    initialize additional  Dinkurs URL info from ENV

--- config/initializers/dinkurs.rb
+++ config/initializers/dinkurs.rb
@@ -1,3 +1,9 @@
-# required to communicate with Dinkurs.se via the DinkursService object
+# Required to communicate with Dinkurs.se via the DinkursService object
+#
+# This gets information from ENV either the .env file or the actual environment via the dotenv gem.
+
+require 'dotenv'
 
 DINKURS_XML_URL =   ENV['DINKURS_XML_URL']
+
+DINKURS_COMPANY_ARG = ENV['DINKURS_URL_COMPANY_ARG']
