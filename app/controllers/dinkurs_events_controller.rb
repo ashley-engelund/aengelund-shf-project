6f39dde74cfdf20425c2ba877ec69f2bf35c8715
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2017-11-29 03:00:21 -0800

    dinkurs controller: finish list of attributes; add authorization

--- app/controllers/dinkurs_events_controller.rb
+++ app/controllers/dinkurs_events_controller.rb
@@ -2,10 +2,10 @@ class DinkursEventsController < ApplicationController
 
   before_action :set_dinkurs_event, only: [:show, :edit, :update, :destroy]
   before_action :authorize_dinkurs_event, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_dinkurs_class, only: [:index, :new, :create, :get_dinkurs_events]
 
 
   def index
-    authorize DinkursEvent
     @dinkurs_events = DinkursEvent.all
   end
 
@@ -15,7 +15,6 @@ class DinkursEventsController < ApplicationController
 
 
   def new
-    authorize DinkursEvent
     @dinkurs_event = DinkursEvent.new
   end
 
@@ -25,7 +24,7 @@ class DinkursEventsController < ApplicationController
 
 
   def create
-    authorize DinkursEvent
+
     @dinkurs_event = DinkursEvent.new(dinkurs_event_params)
 
     respond_to do |format|
@@ -75,12 +74,43 @@ class DinkursEventsController < ApplicationController
   end
 
 
+  def authorize_dinkurs_class
+    authorize DinkursEvent
+  end
+
+
   # Never trust parameters from the scary internet, only allow the white list through.
   def dinkurs_event_params
-    # TODO - finish this list
-    params.require(:dinkurs_event).permit(:event_name, :dinkurs_id, :event_place, :event_place_geometry, :event_host, :event_fee,
-    :event_fee_tax, :event_pub, :event_apply, :event_start, :event_stop,
-    :event_key, :event_url_id, :event_url_key, :company_id)
+    params.require(:dinkurs_event).permit(:event_name,
+                                          :dinkurs_id,
+                                          :event_place,
+                                          :event_place_geometry,
+                                          :event_host,
+                                          :event_fee,
+                                          :event_fee_tax,
+                                          :event_pub,
+                                          :event_apply,
+                                          :event_start,
+                                          :event_stop,
+                                          :event_key,
+                                          :event_participant_number,
+                                          :event_participant_reserve,
+                                          :event_participants,
+                                          :event_occasions,
+                                          :event_group,
+                                          :event_position,
+                                          :event_instructor_1,
+                                          :event_instructor_2,
+                                          :event_instructor_3,
+                                          :event_infotext,
+                                          :event_commenttext,
+                                          :event_ticket_info,
+                                          :event_url_id,
+                                          :event_url_key,
+                                          :event_completion_text,
+                                          :event_aftertext,
+                                          :event_event_dates,
+                                          :company_id)
   end
 
 end
