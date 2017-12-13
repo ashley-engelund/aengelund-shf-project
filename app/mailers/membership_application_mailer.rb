0f9fc0f9cdfaacb93273efcb41ebed7e87b44a6f
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2017-12-12 16:37:34 -0800

    mailer: var for whether or not the branding license fee has been paid

--- app/mailers/membership_application_mailer.rb
+++ app/mailers/membership_application_mailer.rb
@@ -9,6 +9,7 @@ class MembershipApplicationMailer < AbstractMembershipInfoMailer
 
 
   def app_approved(member_application)
+    @branding_fee_paid = member_application.company&.branding_license?
 
     send_mail_for __method__, member_application, t('application_mailer.membership_application.app_approved.subject')
 
