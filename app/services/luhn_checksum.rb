4f76185d8ed948d133b5920784fc1c67686c6032
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2017-12-13 21:55:48 -0800

    Luhn checksum calculation and validation

---
+++ app/services/luhn_checksum.rb
@@ -0,0 +1,67 @@
+# Calculate the Luhn checksum of a number or a string that represents a number
+# Use the Luhn algorithm to calculate the checksum)
+#
+# Return true or false if a string (that represents a number) or number passes the Luhn checksum test
+#
+#
+# @url https://en.wikipedia.org/wiki/Luhn_algorithm
+# @url https://rosettacode.org/wiki/Luhn_test_of_credit_card_numbers
+#
+class LuhnChecksum
+
+  #
+  # Does the last digit of the number meet the Luhn checksum test?
+  #
+  # @param [ Integer | String ] integer_or_str to check
+  # @return Boolean - true if the checksum mod 10 == 0 (meets the test), else false
+  #   returns false for nil
+  #   returns false for an empty string
+  #   returns false if a string does not have any digits
+  #   removes any non-digit characters from the string (INCLUDING a PERIOD!) and then checks the resulting Integer
+  #
+  def self.valid?(integer_or_str)
+
+    # quick checks for the easy cases:
+    return false if integer_or_str.to_s.empty?
+
+    return false if integer_or_str.to_s.scan(/\d/).empty?
+
+    for_number(integer_or_str) % 10 == 0
+  end
+
+
+  # Return the Luhn checksum for a number.
+  # @param [ Integer | String ] integer_or_str
+  # @return Integer - the Luhn checksum number calculated for the (integer representation of) integer_or_str
+  #   if the string is nil or has no digits, return 1 (just need to return any non-zero value)
+  #
+  # checksum = 0
+  # From the rightmost digit, which is the check digit, and moving left:
+  #  double the value of every other (2nd) digit.
+  #    If the result of this doubling operation is greater than 9 (e.g., 8 × 2 = 16),
+  #              then add the digits of the product (e.g., 16: 1 + 6 = 7, 18: 1 + 8 = 9)
+  #              or, alternatively, the same result can be found by subtracting 9 from the product (e.g., 16: 16 − 9 = 7, 18: 18 − 9 = 9).
+  #    add this value to the checksum
+  #  if you aren't doubling the digit, just add it to the checksum.
+  #
+  #
+  # Note the the checksum of 0 = 0
+  #  and  any leading zeros will add 0 to the checksum, so they don't matter
+  #
+  def self.for_number(integer_or_str)
+    return 1 if integer_or_str.nil? || integer_or_str.to_s.scan(/\d/).empty? # nil or if there are no digits
+
+    checksum = 0
+    digits = integer_or_str.to_s.reverse.scan(/\d/).map { |d| d.to_i } # convert to all digits and reverse
+
+
+    digits.each_slice(2) do |odd, even|
+      double = even.to_i * 2
+      double -= 9 if double > 9
+      checksum += double + odd
+    end
+
+    checksum
+  end
+
+end
---
+++ spec/services/luhn_checksum_spec.rb
@@ -0,0 +1,168 @@
+require_relative(File.join(__dir__, '..', '..', 'app', 'services', 'luhn_checksum'))
+
+require 'spec_helper'
+
+
+RSpec.describe LuhnChecksum do
+
+
+  describe 'valid?(x)' do
+
+    it 'false for nil' do
+      expect(described_class.valid?(nil)).to be_falsey
+    end
+
+    it 'false for an empty string' do
+      expect(described_class.valid?('')).to be_falsey
+    end
+
+    it 'false for a string with no digits' do
+      expect(described_class.valid?('just-letters')).to be_falsey
+    end
+
+    describe 'a string that looks like a number with a decimal' do
+
+      describe 'true iff it valid?( the string as an integer with the decimal removed) == TRUE' do
+
+      end
+
+    end
+
+    describe 'integers' do
+      it 'valid?(0) TRUE' do
+        expect(described_class.valid?(0)).to be_truthy
+      end
+
+      it 'valid?(49927398716) TRUE' do
+        expect(described_class.for_number(49927398716)).to be_truthy
+      end
+
+      it 'valid?(49927398717) FALSE' do
+        expect(described_class.valid?(49927398717)).to be_falsey
+      end
+
+      it 'valid?(1234567812345678) FALSE' do
+        expect(described_class.valid?(1234567812345678)).to be_falsey
+      end
+
+      it 'valid?(1234567812345670) TRUE' do
+        expect(described_class.valid?(1234567812345670)).to be_truthy
+      end
+
+    end
+
+    describe 'strings' do
+      it "valid?('0') TRUE" do
+        expect(described_class.valid?('0')).to be_truthy
+      end
+
+      it "valid?('49927398716') TRUE" do
+        expect(described_class.for_number('49927398716')).to be_truthy
+      end
+
+      it "valid?('49927398717') FALSE" do
+        expect(described_class.valid?('49927398717')).to be_falsey
+      end
+
+      it "valid?('1234567812345678') FALSE" do
+        expect(described_class.valid?('1234567812345678')).to be_falsey
+      end
+
+      it "valid?('1234567812345670') TRUE" do
+        expect(described_class.valid?('1234567812345670')).to be_truthy
+      end
+
+    end
+
+  end
+
+
+  describe 'for_number(x)' do
+
+    it '1 for nil' do
+      expect(described_class.for_number(nil)).to eq 1
+    end
+
+    it '1 for an empty string' do
+      expect(described_class.for_number('')).to eq 1
+    end
+
+    it '1 for a string with no digits' do
+      expect(described_class.for_number('just-letters')).to eq 1
+    end
+
+
+    describe 'a string that looks like a number with a decimal' do
+
+      describe 'true iff it luhn_checksum?( the string as an integer with the decimal removed) == TRUE' do
+
+        it "for_number(('.100') = 1" do
+          expect(described_class.for_number('.100')).to eq 1
+        end
+        it "for_number(('1.00') = 1" do
+          expect(described_class.for_number('1.00')).to eq 1
+        end
+        it "for_number(('10.0') = 1" do
+          expect(described_class.for_number('10.0')).to eq 1
+        end
+        it "for_number(('100.') = 1" do
+          expect(described_class.for_number('100.')).to eq 1
+        end
+
+
+        it "for_number(('4.9927398716') = 70" do
+          expect(described_class.for_number('4.9927398716')).to eq 70
+        end
+        it "valid?('4.9927398716') = TRUE" do
+          expect(described_class.valid?('4.9927398716')).to be_truthy
+        end
+
+      end
+
+    end
+
+
+    it 'for_number((0) = 0' do
+      expect(described_class.for_number(0)).to eq 0
+    end
+    it "for_number(('0') = 0" do
+      expect(described_class.for_number('0')).to eq 0
+    end
+
+    it "for_number((100) = 1" do
+      expect(described_class.for_number(100)).to eq 1
+    end
+    it "for_number(('100') = 1" do
+      expect(described_class.for_number('100')).to eq 1
+    end
+    it "for_number(('0100') = 1" do
+      expect(described_class.for_number('0100')).to eq 1
+    end
+
+    it 'for_number((49927398716) = 70' do
+      expect(described_class.for_number(49927398716)).to eq 70
+    end
+    it "for_number(('49927398716') = 70" do
+      expect(described_class.for_number('49927398716')).to eq 70
+    end
+
+    it 'for_number((1234567812345678) = 8' do
+      expect(described_class.for_number(1234567812345678)).not_to eq 8
+    end
+    it "for_number(('1234567812345678') = 8" do
+      expect(described_class.for_number('1234567812345678')).not_to eq 8
+    end
+
+    it "for_number((1234567812345670) = 60" do
+      expect(described_class.for_number(1234567812345670)).to eq 60
+    end
+
+    it "for_number(('1234567812345670') = 60" do
+      expect(described_class.for_number('1234567812345670')).to eq 60
+    end
+
+  end
+
+
+end
+
