7c8c90f93d80807707d712f86bf8d63852d842f2
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2017-12-18 17:29:09 -0800

    MemberMailer - set @member instance var so we can reference it (.company) in the mail views

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -6,7 +6,7 @@ class MemberMailer < ApplicationMailer
   def membership_granted(member)
 
     set_mail_info __method__, member
-
+    @member = member
     mail to: @recipient_email, subject: t('mailers.member_mailer.membership_granted.subject')
 
   end
