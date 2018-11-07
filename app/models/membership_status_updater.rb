d7b573d24992e56de048179a8353e797cff4b5f4
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2018-11-07 12:08:43 -0800

    improve/refine Updaters

--- app/models/membership_status_updater.rb
+++ app/models/membership_status_updater.rb
@@ -1,3 +1,18 @@
+
+# Log messages:
+#
+LOGMSG_APP_UPDATED              = 'SHF_application updated'
+LOGMSG_APP_UPDATED_CHECKREASON  = 'Membership checked because this shf_application was updated: '
+LOGMSG_PAYMENT_MADE             = 'Payment made'
+LOGMSG_PAYMENT_MADE_CHECKREASON = 'Finished checking membership status because this payment was  made: '
+LOGMSG_USER_UPDATED             = 'User updated'
+LOGMSG_USER_UPDATED_CHECKREASON = 'User updated: '
+
+LOGMSG_MEMBERSHIP_GRANTED = 'Membership granted'
+LOGMSG_MEMBERSHIP_RENEWED = 'Membership renewed'
+LOGMSG_MEMBERSHIP_REVOKED = 'Membership revoked'
+
+
 #--------------------------
 #
 # @class MembershipStatusUpdater
@@ -43,10 +58,10 @@
 #
 #--------------------------
 
-
 class MembershipStatusUpdater < AbstractUpdater
 
   SEND_EMAIL_DEFAULT = true
+
 
 
   def self.update_requirements_checker
@@ -67,63 +82,79 @@ class MembershipStatusUpdater < AbstractUpdater
   #
 
   def shf_application_updated(shf_app)
-    ActivityLogger.open(log_filename, self.class.to_s, 'shf_application updated', false) do |log|
-
-      check_requirements_and_act({ user: shf_app.user })
-
-      log.record(:info, "membership checked because this shf_application was updated: #{shf_app.inspect}")
-    end
+    check_user_and_log(user, shf_app, LOGMSG_USER_UPDATED, LOGMSG_USER_UPDATED_CHECKREASON)
   end
 
 
   def payment_made(payment)
-    ActivityLogger.open(log_filename, self.class.to_s, 'payment made', false) do |log|
-
-      check_requirements_and_act({ user: payment.user })
-
-      log.record(:info, "finished checking membership status because this payment was  made: #{payment.inspect}")
-    end
+    check_user_and_log(payment.user, payment, LOGMSG_PAYMENT_MADE, LOGMSG_PAYMENT_MADE_CHECKREASON)
   end
 
 
   def user_updated(user)
-
-    ActivityLogger.open(log_filename, self.class.to_s, 'user updated', false) do |log|
-
-      check_requirements_and_act({ user: user })
-
-      log.record(:info, "User updated: #{user.inspect}")
-    end
-
+    check_user_and_log(user, user, LOGMSG_USER_UPDATED, LOGMSG_USER_UPDATED)
   end
 
 
   private
 
+
+  # check the requirements for the user and log information
+  def check_user_and_log(user, notification_sender, action_message, reason_check_happened)
+
+    ActivityLogger.open(log_filename, self.class.to_s, action_message, false) do |log|
+
+      check_requirements_and_act({ user: user })
+
+      log.record(:info, "#{reason_check_happened}: #{notification_sender.inspect}")
+    end
+  end
+
+
+  # if already a member, then they RENEW, else membership is granted
   def update_action(args)
-    user = args[:user]
+    user       = args[:user]
     send_email = args.fetch(:send_email, SEND_EMAIL_DEFAULT)
 
-    ActivityLogger.open(log_filename, self.class.to_s, "membership granted", false) do |log|
-
-      user.update(member: true, membership_number: user.issue_membership_number) # I don't think a User should be responsible for figuring out the next membership number
-      MemberMailer.membership_granted(user).deliver if send_email
-
-      log.record(:info, "Membership granted for #{user.inspect}")
-    end
-
+    user.member ? renew_membership(user, send_email) : grant_membership(user, send_email)
   end
 
 
   def revoke_update_action(args = {})
     user = args[:user]
-    ActivityLogger.open(log_filename, self.class.to_s, "membership revoked", false) do |log|
-      user.update(member: false)
-      log.record(:info, "Membership revoked for #{user.inspect}")
-      # future:  this makes it easy to record an audit trail here
+    ActivityLogger.open(log_filename, self.class.to_s, LOGMSG_MEMBERSHIP_REVOKED, false) do |log|
+
+      user.update(member: false) # TODO send any email?
+
+      log.record(:info, "#{LOGMSG_MEMBERSHIP_REVOKED}: #{user.inspect}")
+      # future: this makes it easy to record an audit trail here
     end
   end
 
+
+  def grant_membership(user, send_email)
+
+    ActivityLogger.open(log_filename, self.class.to_s, LOGMSG_MEMBERSHIP_GRANTED, false) do |log|
+
+      user.update(member: true, membership_number: user.issue_membership_number) # I don't think a User should be responsible for figuring out the next membership number
+      MemberMailer.membership_granted(user).deliver if send_email
+
+      log.record(:info, "#{LOGMSG_MEMBERSHIP_GRANTED}: #{user.inspect}")
+    end
+
+  end
+
+
+  def renew_membership(user, send_email)
+
+    ActivityLogger.open(log_filename, self.class.to_s, LOGMSG_MEMBERSHIP_RENEWED, false) do |log|
+
+      MemberMailer.membership_renewed(user).deliver if send_email
+
+      log.record(:info, "#{LOGMSG_MEMBERSHIP_RENEWED}: #{user.inspect}")
+    end
+
+  end
 
 end # MembershipStatusUpdater
 
