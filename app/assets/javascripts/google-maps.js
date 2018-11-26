d056c8ece302fdc55aac2716ab2a80126ceb6a14
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2018-11-25 19:45:47 -0800

    WIP: search near me checkbox (= always Stockholm at the moment)

--- app/assets/javascripts/google-maps.js
+++ app/assets/javascripts/google-maps.js
@@ -12,12 +12,20 @@
 //  else display all of the markers,and so the center is automatically
 //   determined by the center of all of them.
 //
-function initCenteredMap(centerCoordinates, markers, icon) {
+//  We need to tell GoogleMaps whether or not we want it to be optimized or not.
+//  If it _is_ optimized, then the map is just a canvas and we will not be able to
+//  test for specific marker or other elements on it.
+//  If it is _not_ optimized, then we will be able to test for specific elements;
+//  we need to do this if we are developing or testing.
+//
+//  @url https://mixandgo.com/learn/how-to-write-a-cucumber-test-for-google-maps
+//
+function initCenteredMap(centerCoordinates, markers, icon, nearMeCheckvalue, doOptimize) {
 
     var mapCenter = {lat: 59.3293235, lng: 18.0685808};
 
     var marks = markers === null ? [] : markers;
-
+    var optimize = doOptimize === null ? false : doOptimize;
 
     if (centerCoordinates === null) {
         // try to get the user's coordinates
@@ -27,19 +35,21 @@ function initCenteredMap(centerCoordinates, markers, icon) {
                     position.coords.longitude);
             });
         }
-    }
-    else {
+    } else {
         mapCenter = centerCoordinates;
     }
 
     var map = new google.maps.Map(document.getElementById('map'), {
         center: mapCenter,
-        zoom: 13
+        zoom: 13,
+
+        mapTypeControl: false  // hide this control to save space.
+
     });
 
     var bounds = new google.maps.LatLngBounds();
 
-    addMarkersToMap(map, marks, bounds, icon);
+    addMarkersToMap(map, marks, bounds, icon, optimize);
 
     //now fit the map to the newly inclusive bounds
     // this will zoom in too far if there's only 1 marker
@@ -47,11 +57,18 @@ function initCenteredMap(centerCoordinates, markers, icon) {
         map.fitBounds(bounds);
     }
 
+    // Create the DIV to hold the search near me button and call the SearchNearMeCheckbox()
+    // constructor passing in this DIV.
+    var searchNearMeCheckboxDiv = document.createElement('div');
+    SearchNearMeCheckbox(searchNearMeCheckboxDiv, nearMeCheckvalue);
+    searchNearMeCheckboxDiv.index = 1;
+
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(searchNearMeCheckboxDiv);
 }
 
 
 // add the markers to the map that is defined on the page with id = 'map'
-function addMarkersToMap(map, markers, bounds, icon) {
+function addMarkersToMap(map, markers, bounds, icon, optimize) {
 
     // if the map doesn't exist, do nothing
     if (document.getElementById('map') !== null) {
@@ -67,7 +84,7 @@ function addMarkersToMap(map, markers, bounds, icon) {
                 lng: marks[i].longitude
             };
 
-            addMarker(position, map, marks[i].text, icon);
+            addMarker(position, map, marks[i].text, icon, optimize);
 
             //extend the bounds to include the position for this marker
             bound.extend(position);
@@ -104,15 +121,15 @@ function getNumber(elementId) {
 
 // Create a marker. Optionally, set the icon to be used for it
 // When it's clicked, pop-up a box with text in it
-function addMarker(coordinates, map, text, icon) {
+function addMarker(coordinates, map, text, icon, optimize) {
     var marker;
 
     marker = new google.maps.Marker({
         position: coordinates,
         map: map,
-        icon: icon
+        icon: icon,
+        optimized: optimize
     });
-
 
     // don't create a pop-up box if there's no text to display
     if (text !== "") {
@@ -129,5 +146,72 @@ function addMarker(coordinates, map, text, icon) {
 function createInfoWindow(text) {
     return new google.maps.InfoWindow({
         content: text
+    });
+}
+
+
+/**
+ * The SearchNearMeCheckbox adds a checkbox to the given DIV (which should be in a map)
+ * that will
+ * 1) geolocate the user and then
+ * 2) send an AJAX request to the server to search near the user's location.
+ *
+ * If the user can not be geolocated (perhaps they have that option turned off in
+ * their browser), then....?
+ *
+ * When the checkbox is checked:
+ * 1. The companies displayed on the map will be XXX km. from the user's location
+ * 2. Any futher filters (e.g. categories) will use that limited group of companies
+ *
+ * When the checkbox is unchecked:
+ * 1. The companies displayed on the map will not be limited by distance.  They may
+ * still be limited/filtered by other conditions (e.g. categories, etc.)
+ *
+ * @param controlDiv - a DIV that the checkbox gets appended to
+ * @param isChecked [Boolean] - whether or not the checkbox should be checked
+ * @constructor
+ */
+function SearchNearMeCheckbox(controlDiv, isChecked) {
+
+    // outer box that holds the checkbox
+    var searchNearMeDiv = document.createElement("DIV");
+    searchNearMeDiv.id = "searchNearMe";
+
+    var searchNearMeCheckbox = document.createElement("INPUT");
+    searchNearMeCheckbox.setAttribute("type", "checkbox");
+    searchNearMeCheckbox.id = 'searchNearMeCheckbox';
+    searchNearMeCheckbox.className = 'searchNearMe';
+    searchNearMeCheckbox.title = I18n.t("companies.index.search_near_me_title");
+
+    searchNearMeCheckbox.checked = isChecked;
+
+    // text for the checkbox
+    var checkboxLabel = document.createElement("LABEL");
+    checkboxLabel.control = searchNearMeCheckbox;
+    checkboxLabel.id = "checkboxLabel";
+    checkboxLabel.innerText = I18n.t("companies.index.search_near_me");
+
+    searchNearMeDiv.appendChild(searchNearMeCheckbox);
+    searchNearMeDiv.appendChild(checkboxLabel);
+    controlDiv.appendChild(searchNearMeDiv);
+
+    // Setup the click event listeners: simply set the map to Stockholm.
+    searchNearMeCheckbox.addEventListener('click', function () {
+
+        let parsedUrl = new URL(window.location.href);
+        let searchParams = parsedUrl.searchParams.toString();
+
+        if ( this.checked ) {
+            nearParams = "&near=lat=59.3293235,long=18.0685808,dist=100";
+        } else {
+            nearParams = "";
+        }
+
+        $.ajax({
+            url: "hundforetag",
+            type: "GET",
+            data: searchParams + nearParams
+        });
+
     });
 }
