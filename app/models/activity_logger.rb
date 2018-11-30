d540b7dcef70663c58a6ba8412c2e3377a1b4470
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2018-11-29 18:31:53 -0800

    can now log to $stdout; + method to use verified output stream

--- app/models/activity_logger.rb
+++ app/models/activity_logger.rb
@@ -2,7 +2,14 @@ class ActivityLogger
 
   # This supports simple logging of activity, such as loading data into the
   # the DB with a rake task.
-  # This also allows for output to STDOUT of logged messages.
+  #
+  # This allows for output to $stdout of logged messages.
+  #
+  # This will write to $stdout *instead of a file* if:
+  #   ENV['RAILS_LOG_TO_STDOUT'] exists (the value doesn't matter)
+  # or
+  #   if the directory for the log is not writeable or cannot be created
+  #
   #
   # The format of the logged messages are:
   #
@@ -26,15 +33,18 @@ class ActivityLogger
   # 3) when the activity is complete,
   #    -- the log file will be closed automatically if opened with a block, OR
   #    -- call log.close
+  #
+  # NOTE: this requires ActiveSupport.  It can only be run with Rails
 
   def self.open(filename, facility, activity, show=true)
+
     log = new(filename, facility, activity, show)
 
     if block_given?
       begin
         yield log
       ensure
-        log.close
+        log.close unless @is_system_outstream
       end
     else
       log
@@ -48,8 +58,11 @@ class ActivityLogger
     @facility_and_action = "[#{facility}] [#{activity}] "
     @show = show
     @start_time = Time.zone.now
+    @is_system_outstream = false # true if we use $stdout or $stderr
 
-    @log = ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new(filename))
+    verified_output_stream = ActivityLogger.verified_output_stream(filename)
+
+    @log = ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new(verified_output_stream))
 
     record('info', "Started at #{@start_time}")
   end
@@ -70,12 +83,70 @@ class ActivityLogger
     record('info', "Finished at #{finish_time}.")
     record('info',
       "Duration: #{(finish_time - @start_time).round(2)} seconds.\n") if duration
-    @log.close
+    @log.close if @log.respond_to?(:close) && !@is_system_outstream
     logged_to if @show
   end
 
   def logged_to
     puts @facility_and_action + '[info] ' + "Information was logged to: #{@filename}"
   end
+
+
+  # Return a stream to use that we have verified is writeable.
+  #
+  # Given a full filename path, ensure that we can write to the directory.
+  # If not, use $stdout instead.
+  # If the directory for _filename_ does not exist, try to create it.
+  # If we cannot create the directory, then use $stdout for the output.
+  # If the directory is not writeable, then use $stdout for the output.
+  #
+  # @param unverified_filename [String] - the full path of the unverified filename; we
+  #   try to verify that the directory for it exists and is writeable
+  #
+  # @return the full filename + path to use for output.
+  #      This might be changed to $stdout if we could not use or create the directory
+  #      for filename
+  def self.verified_output_stream(unverified_filename)
+
+    @is_system_outstream = true
+
+    # always log to stdout no matter what unverified_filename is
+    if ENV.has_key?('RAILS_LOG_TO_STDOUT')
+      return $stdout
+    end
+
+    # quick gating conditions:
+    return unverified_filename if unverified_filename == $stdout
+    return unverified_filename if unverified_filename == $stderr
+    return unverified_filename if unverified_filename.class.name == 'IO'
+      # sometimes a stdout will not have the same address as $stdout depending on
+      # how it was created
+
+    # if we're here, we didn't explicitly use a system output stream
+    @is_system_outstream = false
+
+    verified_output = $stdout # fallback to this unless we can verify the directory
+
+    unverified_dir = File.dirname unverified_filename
+
+    # if it exists and we can write to it, it's fine to use.
+    if File.exists?(unverified_dir) && File.writable?(unverified_dir)
+      verified_output = unverified_filename
+
+    else  # try to verify it
+      begin
+        Dir.mkdir(unverified_dir) unless File.exists? unverified_dir
+        raise IOError unless File.writable? unverified_dir
+        verified_output = unverified_filename # if we got this far it's fine
+      rescue => _error
+        # Swallow this error; don't raise it.
+        # If we can't create or write to the directory, then this
+        # directory is not verified.  Use the fallback output ($stdout)
+      end
+    end
+
+    verified_output
+  end
+
 
 end
