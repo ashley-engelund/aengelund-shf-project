820b3f932ed83890183099da892617559e6ed897
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2018-12-11 17:44:35 -0800

    add method to PaymentUtility so creating test data is easier

--- app/models/concerns/payment_utility.rb
+++ app/models/concerns/payment_utility.rb
@@ -29,9 +29,16 @@ module PaymentUtility
         start_date = Time.zone.today
       end
 
-      expire_date = start_date + 1.year - 1.day
+      expire_date = expire_date_for_start_date(start_date)
 
       [start_date, expire_date]
     end
+
+
+    # THIS IS THE KEY RULE ABOUT WHEN PAYMENTS EXPIRE:
+    def expire_date_for_start_date(start_date)
+      start_date + 1.year - 1.day
+    end
+
   end
 end
