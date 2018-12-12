bd6aca40b119fd06c04d2f9f239a32f1efeeff84
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2018-12-11 19:28:02 -0800

    Parent class for all alerts that send out emails to a user

---
+++ app/models/conditions_response/user_email_alert.rb
@@ -0,0 +1,116 @@
+# Abstract parent class for all alerts that send out emails to users
+#
+#  * knows if an alert needs to be sent today (or on _this_date_)
+#  * knows the name of the method to send to MemberMailer to send out the
+#     alert to a user
+#
+#  TODO If each class is responsible for doing 1 thing -- i.e. sending out 1
+#   kind of alert for 1 condition, then is 'condition' really needed?
+#     (If/when we have situations when 1 class needs to handle more than 1
+#      condition _and_ we must have it handled only within 1 class,
+#        then we can create the condition attribute.)
+#
+class UserEmailAlert
+
+
+  # TODO this class is an instantiation of the Condition; why pass it again as an arg?
+  def self.condition_response(_condition, config, log, this_date: DateTime.now.utc)
+
+    User.all.each do |user|
+
+      if send_alert_today?(config, user, this_date)
+        MemberMailer.send(mailer_method, user)
+        log.record('info', log_message(log_msg_start, user.email))
+      end
+
+    end
+
+  end
+
+
+  # method to improve readability. returns true if day_number is in config[:days]
+  #
+  # Expects config to include the :days key; returns false if it is not there
+  #
+  # If config is not a Hash, raises an Error because it really _should_ be a Hash
+  #  (it's a programming error to call this otherwise!)
+  #
+  def self.send_on_day_number?(day_number, config)
+    config.fetch(:days, false) ? config[:days].include?(day_number) : false
+  end
+
+
+  # Subclasses should define this by checking information for the user
+  # computing whatever date information is necessary to determine if
+  # an alert should be sent out today.
+  # Ex:
+  #   def self.send_alert_today?(config, user)
+  #     days_until = (user.membership_expire_date - Date.current).to_i
+  #     user.membership_current? &&  config[:days].include?(days_until)
+  #   end
+  #
+  #
+  # @param config [Object] - configuration information used to determine if an alert should be sent
+  # @param user [User] - the user we are checking and, if appropriate, will send the alert to
+  # @param this_date [DateTime] - (defaults to today) the date that we are using
+  #                      to check the condition.  in UTC
+  #
+  def self.send_alert_today?(_config, _user, _this_date = DateTime.now.utc)
+    raise NoMethodError, "Subclass must define the #{__method__} method and return true or false", caller
+  end
+
+
+  # This is the method sent to the MemberMailer when this condition
+  # needs to send out an email.
+  #
+  # Subclasses must redefine this and return a symbol
+  # Ex:
+  #   def self.mailer_method
+  #     :membership_expiration_reminder
+  #   end
+  def self.mailer_method
+    raise NoMethodError, "Subclass must define the #{__method__} method and return a Symbol", caller
+  end
+
+
+  # This string is the start of the line logged to the Alert log when
+  # this condition sends out an email = the name of the class
+  def self.log_msg_start
+    self.name
+  end
+
+
+  def self.log_message(message_start = '', user_email = '')
+    "#{message_start} alert sent to #{user_email}"
+  end
+
+
+  SECONDS_IN_A_DAY = 86400 # 60 * 60 * 24
+
+  # (Utility method for working with dateTimes (helps readability))
+  #
+  # The whole number of days since time1 up to time2 (time2 - time1),
+  # rounding UP to the next whole number of days.
+  # Ex:
+  #     days_since( Thu, 01 Nov 2018 00:00:00 UTC +00:00,
+  #                 Thu, 02 Nov 2018 00:00:01 UTC +00:00 )
+  #    will return 2 days because that's 1 day and 1 minute.
+  #
+  # This is because this method is using to determine things like "is a payment late?"
+  # and if the payment is 1 minute past due, then it is late.  So it makes sense
+  # to round up to the next whole day.
+  #
+  #
+  # @param time1 [Time] - the starting point: days since this time
+  # @param time2 [Time] (optional; defaults to Time.utc)
+  #                              - the date we want to move forward (or backwards) to
+  #
+  # @return the number of days between 2 Times (time2 - time1)
+  def self.days_since(time1, time2 = Time.zone.now)
+    daystart2 = time2.beginning_of_day
+    daystart1 = time1.beginning_of_day
+
+    (daystart2 - daystart1).to_i / SECONDS_IN_A_DAY
+  end
+
+end
---
+++ spec/models/conditions_response/user_email_alert_spec.rb
@@ -0,0 +1,202 @@
+require 'rails_helper'
+
+require 'timecop'
+
+LOG_DIR      = 'tmp'
+LOG_FILENAME = 'testlog.txt'
+
+
+RSpec.describe UserEmailAlert, type: :model do
+
+  let(:user) { create(:user) }
+
+  let(:config) { { days: [2, 5, 10] } }
+
+  # create a double for a condition that responds with a config
+  let(:condition) { create(:condition, config: { days: [2, 5, 10] }) }
+
+
+  let(:filepath) { File.join(Rails.root, LOG_DIR, LOG_FILENAME) }
+  let(:log) { ActivityLogger.open(filepath, 'TEST', 'open', false) }
+
+
+  before(:each) do
+    File.delete(filepath) if File.file?(filepath)
+  end
+
+  after(:all) do
+    tmpfile = File.join(Rails.root, LOG_DIR, LOG_FILENAME)
+    File.delete(tmpfile) if File.exist?(tmpfile)
+  end
+
+
+  describe '.condition_response' do
+
+    before(:all) do
+
+      # define a method for MemberMailer just for this test
+      MemberMailer.class_eval do
+        def fake_mailer_method(_user)
+          true
+        end
+      end
+
+    end
+
+
+    after(:all) do
+      # remove the method we added
+      MemberMailer.undef_method(:fake_mailer_method)
+    end
+
+
+    let(:dec_1) { Time.zone.local(2018, 12, 1) }
+
+
+    it 'for each User: sends alert and logs if send_alert_today? is true' do
+
+      expect(MemberMailer.fake_mailer_method(user)).to be_truthy
+
+
+      # stubbed methods:
+      allow(described_class).to receive(:mailer_method).and_return(:fake_mailer_method)
+
+      allow(described_class).to receive(:send_alert_today?)
+                                    .with(config, user, dec_1)
+                                    .and_return(true)
+
+      allow(described_class).to receive(:log_msg_start).and_return('UserEmailAlert')
+
+
+      # expected results:
+      expect(MemberMailer).to receive(:fake_mailer_method).with(user)
+                                  .exactly(1).times
+
+      expect(described_class).to receive(:send_alert_today?)
+                                     .with(config, user, dec_1)
+
+      expect(log).to receive(:record)
+                         .exactly(1 + 2).times
+                         .and_call_original
+
+
+      Timecop.freeze(dec_1) do
+        described_class.condition_response(nil, config, log)
+        log.close
+      end # Timecop
+
+      expect(File.read(filepath)).to include "[info] UserEmailAlert alert sent to #{user.email}"
+
+    end
+
+    it 'does nothing when send_alert_today? is false for a user' do
+
+      # stubbed methods:
+      allow(described_class).to receive(:send_alert_today?)
+                                    .with(config, user, dec_1)
+                                    .and_return(false)
+
+      allow(described_class).to receive(:mailer_method).and_return(:fake_mailer_method)
+
+
+      # expected results:
+      expect(MemberMailer).not_to receive(:fake_mailer_method).with(user)
+
+      expect(described_class).to receive(:send_alert_today?)
+                                     .with(config, user, dec_1)
+
+      expect(log).to receive(:record)
+                         .exactly(2).times
+                         .and_call_original
+
+
+      # actual test:
+      Timecop.freeze(dec_1) do
+        described_class.condition_response(nil, config, log)
+        log.close
+      end # Timecop
+
+      expect(File.read(filepath)).not_to include "[info] UserEmailAlert alert sent to #{user.email}"
+
+    end
+
+  end
+
+
+  describe '.log_message' do
+
+    it 'creates the string to write out to the log' do
+      expect(described_class.log_message('UserEmailAlert', 'email@example.com')).to eq "UserEmailAlert alert sent to email@example.com"
+    end
+
+    it 'default message start is an empty string' do
+      expect(described_class.log_message(nil, 'email@example.com')).to eq " alert sent to email@example.com"
+    end
+
+    it 'default email is an empty string' do
+      expect(described_class.log_message('UserEmailAlert', nil)).to eq "UserEmailAlert alert sent to "
+    end
+  end
+
+
+  describe '.days_since = number of whole days since a Time, rounded UP' do
+
+    let(:nov_1) { Time.zone.local(2018, 11, 1) }
+    let(:oct_1) { Time.zone.local(2018, 10, 1) }
+    let(:also_oct_1) { Time.zone.local(2018, 10, 1) }
+
+    it 'returns positive if date_time1 is before date_time2' do
+      expect(described_class.days_since(oct_1, nov_1)).to be 31
+    end
+
+    it 'returns negative if date_time1 is after date_time2' do
+      expect(described_class.days_since(nov_1, oct_1)).to be -31
+    end
+
+    it 'returns 0 if the DateTimes are the same' do
+      expect(described_class.days_since(oct_1, also_oct_1)).to be 0
+    end
+
+    it 'returns the days since Time.zone.local if only the first arg is given' do
+      Timecop.freeze(Time.zone.local(2018, 12, 1)) do
+        expect(described_class.days_since(nov_1)).to be 30
+      end # Timecop
+    end
+
+  end
+
+
+  describe '.send_on_day_number?' do
+
+    let(:config) { { days: [1, 3, 5] } }
+
+    it 'true if config[:days].include? day_number' do
+      expect(described_class.send_on_day_number?(3, config)).to be_truthy
+    end
+
+    it 'false if day_number is not in config[:days]' do
+      expect(described_class.send_on_day_number?(0, config)).to be_falsey
+    end
+
+    it 'false if config does not have :days as a key' do
+      expect(described_class.send_on_day_number?(3, { blorf: 'blorf' })).to be_falsey
+    end
+
+  end
+
+
+  it '.log_msg_start is the class name' do
+    expect( described_class.log_msg_start ).to eq described_class.name
+  end
+
+
+  it '.send_alert_today?(_config, _user) raises NoMethodError (should be defined by subclasses)' do
+    config = {}
+    expect { described_class.send_alert_today?(config, user) }.to raise_exception NoMethodError
+  end
+
+  it '.mailer_method raises NoMethodError (should be defined by subclasses)' do
+    expect { described_class.mailer_method }.to raise_exception NoMethodError
+  end
+
+end
