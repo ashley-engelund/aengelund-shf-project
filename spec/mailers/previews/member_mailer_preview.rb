aab3926bfde2a7518b74897a8e54569e717f334a
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-01-31 11:58:58 -0800

    remove FactoryBot creation from the preview method. (it caused too much weirdness)

--- spec/mailers/previews/member_mailer_preview.rb
+++ spec/mailers/previews/member_mailer_preview.rb
@@ -6,31 +6,48 @@ class MemberMailerPreview < ActionMailer::Preview
 
   include PickRandomHelpers
 
+
   def membership_granted
     approved_app = ShfApplication.where(state: :accepted).first
     MemberMailer.membership_granted(approved_app.user)
   end
+
 
   def membership_expiration_reminder
     member = User.where(member: true).first
     MemberMailer.membership_expiration_reminder(member)
   end
 
+
   def h_branding_fee_past_due
 
-    new_email = "user-#{Time.now.to_i}@example.com"
+    new_email         = "user-#{Time.now.to_i}@example.com"
     new_approved_user = FactoryBot.create(:member_with_membership_app, email: new_email)
-    new_co = new_approved_user.shf_application.companies.first
+    new_co            = new_approved_user.shf_application.companies.first
 
     MemberMailer.h_branding_fee_past_due(new_co, new_approved_user)
   end
 
 
   def app_no_uploaded_files
-    new_email = "user-#{Time.now.to_i}@example.com"
-    new_approved_user = FactoryBot.create(:user_with_membership_app, email: new_email)
 
-    MemberMailer.app_no_uploaded_files new_approved_user
+    # create a new user with a brand new application (that has no uploaded files)
+    if @new_approved_user.nil?
+      new_email = "sussh-#{Time.now.to_i}@example.com"
+
+      @new_approved_user = User.create(first_name:   'Suss',
+                                       last_name:    'Hundapor',
+                                       password:     'whatever',
+                                       email:        new_email,
+                                       member_photo: nil,
+      )
+      ShfApplication.new(user: @new_approved_user)
+    end
+
+    MemberMailer.app_no_uploaded_files @new_approved_user
+
+  ensure
+    User.delete(@new_approved_user.id) unless @new_approved_user.nil?
   end
 
 end
