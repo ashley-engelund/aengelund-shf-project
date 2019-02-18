3ca1d1bcf34cf31d4224fd78f14167c02eb80745
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-02-18 03:10:10 -0800

    preview a new app with no files uploaded; new app with uploads

--- spec/mailers/previews/shf_application_mailer_preview.rb
+++ spec/mailers/previews/shf_application_mailer_preview.rb
@@ -11,9 +11,23 @@ class ShfApplicationMailerPreview < ActionMailer::Preview
   end
 
 
-  def acknowledge_received
-    ShfApplicationMailer.acknowledge_received(random_shf_app)
+  def acknowledge_received_no_files_uploaded
+    no_uploads = ShfApplication.where.not(id: [UploadedFile.pluck(:shf_application_id)])
+    if no_uploads.count == 0
+      # create an application with no uploads by removing all the uploads from an app
+      new_app = ShfApplication.where(state:'new').first
+      new_app.uploaded_files.delete_all
+      shf_app_no_uploads = new_app
+    else
+      shf_app_no_uploads = no_uploads.first
+    end
+    ShfApplicationMailer.acknowledge_received(shf_app_no_uploads)
   end
 
+
+  def acknowledge_received_with_files_uploaded
+    app_with_uploads = ShfApplication.joins(:uploaded_files).first
+    ShfApplicationMailer.acknowledge_received(app_with_uploads)
+  end
 
 end
