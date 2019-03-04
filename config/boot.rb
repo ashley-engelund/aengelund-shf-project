1ae0abfe51f4b68d9249dca0a8ab7c6bb303f324
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-03-04 14:23:26 -0800

    improve comment: refer to the exact text in the line

--- config/boot.rb
+++ config/boot.rb
@@ -5,7 +5,7 @@ require 'bundler/setup' # Set up gems listed in the Gemfile.
 
 # Note: 2019-02-23: In ruby 2.5.1 and 2.5.2 (fixed in 2.5.3)
 # IDE debugger (RubyMine) will not stop at breakpoints in Controller classes
-# unless you comment out the 'require' line for it in config/boot.rb
+# unless you comment out the "require 'bootsnap/setup'" line below
 #
 # Bug in ruby: https://bugs.ruby-lang.org/issues/14702
 #   see discussion in rails/rails for more info and this workaround:
