0385b1bb1cea706f258c95055939f6401c342684
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-03-06 01:48:40 -0800

    requirements for HBranding fee will be due and will not be due

---
+++ app/models/reqs_updaters/requirements_for_h_branding_fee_will_not_be_due.rb
@@ -0,0 +1,28 @@
+#--------------------------
+#
+# @class RequirementsForHBrandingFeeWillNotBeDue
+#
+# @desc Responsibility: Knows when an H-Branding Fee will NOT be due for a company
+# (= the requirements are met)
+#
+#       This is a very simple class because the requirements are currently very simple.
+#       The importance is that
+#        it is the only place that code needs to be touched if the rules for
+#        when an H-Branding fee will _not_ be due are changed.
+#
+#  Only 1 is needed for the system.
+#
+# @author Ashley Engelund (ashley@ashleycaroline.com  weedySeaDragon @ github)
+# @date   2019-03-05
+# @file requirements_for_h_branding_fee_not_due.rb
+#
+#--------------------------
+
+
+class RequirementsForHBrandingFeeWillNotBeDue < AbstractOppositeRequirements
+
+  def self.opposite_class
+    RequirementsForHBrandingFeeWillBeDue
+  end
+
+end
