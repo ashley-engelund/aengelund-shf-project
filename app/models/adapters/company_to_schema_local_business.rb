ed4a14ede29c6cb9954fe3b5f7d795c425ab44d2
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-03-09 00:20:11 -0800

    simplify: reduce cohesion by using util classes

--- app/models/adapters/company_to_schema_local_business.rb
+++ app/models/adapters/company_to_schema_local_business.rb
@@ -2,33 +2,32 @@
 
 require_relative File.join(__dir__, '../concerns/company_hmarkt_url_generator')
 
-#--------------------------
-#
-# @class Adapters::CompanyToSchemaLocalBusiness
-#
-# @desc Responsibility: (an Adapter) takes a Company and creates a
-#   schema.org local business
-#   Note that we need to create a local business so that we can have multiple
-#   locations (places).
-#
-#     Pattern: Adapter
-#       @adaptee: Company
-#       target: SchemaDotOrg::LocalBusiness  https://schema.org/LocalBusiness
-#
-#
-# @author Ashley Engelund (ashley.engelund@gmail.com  weedySeaDragon @ github)
-# @date   2019-02-19
-#
-# @file company_to_schema_local_business.rb
-#
-#--------------------------
 
 module Adapters
 
+  #--------------------------
+  #
+  # @class Adapters::CompanyToSchemaLocalBusiness
+  #
+  # @desc Responsibility: (an Adapter) takes a Company and creates a
+  #   schema.org local business
+  #   Note that we need to create a local business so that we can have multiple
+  #   locations (places).
+  #
+  #     Pattern: Adapter
+  #       @adaptee: Company
+  #       target: SchemaDotOrg::LocalBusiness  https://schema.org/LocalBusiness
+  #
+  #
+  # @author Ashley Engelund (ashley.engelund@gmail.com  weedySeaDragon @ github)
+  # @date   2019-02-19
+  #
+  # @file company_to_schema_local_business.rb
+  #
+  #--------------------------
   class CompanyToSchemaLocalBusiness < AbstractSchemaOrgAdapter
 
     include CompanyHMarktUrlGenerator
-
 
     def target_class
       SchemaDotOrg::LocalBusiness
@@ -44,17 +43,14 @@ module Adapters
       target.telephone   = @adaptee.phone_number
       target.image       = company_h_markt_url(@adaptee) # FIXME - this needs to be a permanent image and URL
 
-      # this may set many properties, so we work with the target
-      target = set_address_properties(target, @adaptee)
+      target = AddressesIntoSchemaLocalBusiness.set_address_properties(@adaptee.addresses, target)
 
       target.knowsLanguage = 'sv-SE'
 
-      # this just sets 1 property
-      target.knowsAbout = set_knows_about(@adaptee)
+      target.knowsAbout = BusinessCatsIntoKnowsString.knows_str(@adaptee.business_categories)
 
       target
     end
-
 
 
     def url_for_co_hbrand_image(company)
@@ -62,45 +58,6 @@ module Adapters
     end
 
 
-    # ==========================================================================
-    # ==========================================================================
-
-
-    private
-
-
-    def set_address_properties(target, adaptee)
-      unless adaptee.addresses.empty?
-        target.address = AddressToSchemaPostalAddress.new(adaptee.addresses.first).as_target
-        target.geo     = AddressToSchemaGeoCoordinates.new(adaptee.addresses.first).as_target
-
-        # for multiple addresses, list multiple locations, each with an address and geo coordinates
-        if adaptee.addresses.size > 1
-
-          target.location = []
-          adaptee.addresses.each do |address|
-            target.location << AddressToSchemaPlace.new(address).as_target
-          end
-        end
-
-      end
-
-      target
-    end
-
-
-    # set the "knowsAbout" property to the list of business categories for the company
-    def set_knows_about(adaptee)
-
-      return nil if adaptee.business_categories.empty?
-
-      knows_about = []
-      adaptee.business_categories.each do |category|
-        knows_about << "#{I18n.t('dog').capitalize} #{category.name}"
-      end
-
-      knows_about
-    end
   end
 
 end
