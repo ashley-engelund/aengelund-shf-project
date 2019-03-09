314610acff30aecd6814f9956b01a26ba684c7e3
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-03-08 23:04:56 -0800

    put block comments immediately above class

--- app/models/schema_dot_org/place.rb
+++ app/models/schema_dot_org/place.rb
@@ -1,23 +1,23 @@
 #!/usr/bin/ruby
 
-#--------------------------
-#
-# @class Place
-#         https://schema.org/Place
-#   This is just a subset of the properties of the schema.org Place type
-#
-# @desc Responsibility: "Entities that have a somewhat fixed, physical extension."
-#
-#
-# @author Ashley Engelund (ashley.engelund@gmail.com  weedySeaDragon @ github)
-# @date   2019-02-19
-#
-# @file place.rb
-#
-#--------------------------
 
 module SchemaDotOrg
 
+  #--------------------------
+  #
+  # @class Place
+  #         https://schema.org/Place
+  #   This is just a subset of the properties of the schema.org Place type
+  #
+  # @desc Responsibility: "Entities that have a somewhat fixed, physical extension."
+  #
+  #
+  # @author Ashley Engelund (ashley.engelund@gmail.com  weedySeaDragon @ github)
+  # @date   2019-02-19
+  #
+  # @file place.rb
+  #
+  #--------------------------
   class Place < Thing
 
     # address is a PostalAddress, geo is a GeoCoordinates
