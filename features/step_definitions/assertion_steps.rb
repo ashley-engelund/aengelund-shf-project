54682c9660e4c26cb8f847c243362e8d01a63ef6
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-03-27 13:47:07 -0700

    correct and separate 'user account' and 'user profile' in steps

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -58,8 +58,10 @@ module PathHelpers
         path = shf_documents_path
       when 'new shf document'
         path = new_shf_document_path
-      when 'user details', 'user profile'
+      when 'user details', 'user account'
         path = user_path(user)
+      when 'user profile'
+        path = edit_user_registration_path(user)
       when 'test exception notifications'
         path = test_exception_notifications_path
       when 'admin dashboard'
