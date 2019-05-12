5c984f407501dd1ceb52c39582c6f19ca32cffb4
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-05-12 12:37:17 -0700

    update comments

--- app/controllers/concerns/custom_devise_fail_app.rb
+++ app/controllers/concerns/custom_devise_fail_app.rb
@@ -1,11 +1,10 @@
-
 #--------------------------
 #
 # @class CustomDeviseFailApp
 #
-# @desc Responsibility: Redirects users to the login page on a Warden auth failure
-#
-#
+# @desc Responsibility: Redirects users to the login page on Warden authorization failures
+# See
+# https://github.com/plataformatec/devise/wiki/How-To:-Redirect-to-a-specific-page-when-the-user-can-not-be-authenticated)
 #
 # @author Ashley Engelund (ashley.engelund@gmail.com  weedySeaDragon @ github)
 # @date   2019-05-11
@@ -15,16 +14,13 @@
 #--------------------------
 class CustomDeviseFailApp < Devise::FailureApp
 
-  # Where users will be redirected on any Warden failure
-  # = login path
+  # Redirect users to the login path on a Warden authorization failure
   def redirect_url
     new_user_session_path
   end
 
 
-  # Need to override this so that warden recall is not involved (per Devise
-  # FailuraApp instructions:
-  # https://github.com/plataformatec/devise/wiki/How-To:-Redirect-to-a-specific-page-when-the-user-can-not-be-authenticated)
+  # Need to override this so that warden recall is not involved
   def respond
     if http_auth?
       http_auth
