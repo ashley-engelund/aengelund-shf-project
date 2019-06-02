56b6f24d50c9a0c6e0ec9b2ad06c0ff27ad2386d
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-06-01 20:25:43 -0700

    simplify with a shared example for testing nested/previous error

--- spec/features/support/cucumber_steps_errors_spec.rb
+++ spec/features/support/cucumber_steps_errors_spec.rb
@@ -3,43 +3,53 @@ require 'rails_helper'
 require_relative File.join(Rails.root, 'features', 'support', 'step_errors')
 
 
+# --------------------------------------------
+
+RSpec.shared_examples 'it shows the previous error when this one is raised (nested errors)' do | raised_shf_error |
+
+  it 'previous error message is displayed so the context is clear' do
+    begin
+      # Artificially create a previous error that has happened
+      raise NoMethodError
+
+    rescue NoMethodError
+      # Create (raise) one of our errors (some SHFStepsError)
+      begin
+        raise raised_shf_error
+      rescue raised_shf_error.class => shf_error
+        # This is the rescue statement that will catch our error
+        # This is the rescue statement that will happen in real code.
+      end
+    end
+
+    expect(shf_error.message).to match(/This was raised after this error: NoMethodError\./)
+  end
+end
+
+# --------------------------------------------
+
 RSpec.describe 'Cucumber Steps Errors' do
 
   describe 'SHFStepsError' do
 
     let(:error_class) { SHFStepsError }
 
-    it 'will show the message if a previous error raised' do
-
-      begin
-        raise NoMethodError
-      rescue NoMethodError
-        shf_error = error_class.new
-      end
-
-      expect(shf_error.message).to eq "SHFStepsError: This was raised after this error: NoMethodError."
-    end
-
+    it_behaves_like 'it shows the previous error when this one is raised (nested errors)', SHFStepsError.new
 
     it 'shows just the class name if no previous error was raised' do
-
       begin
+        # Create (raise) one of our errors (some SHFStepsError)
         raise error_class
+
       rescue NoMethodError, error_class
+        # This is the rescue statement that will catch one of our errors (SHFStepsError).
+        # This is the rescue statement that will happen in real code.
         shf_error = error_class.new
       end
 
       expect(shf_error.message).to eq "SHFStepsError"
     end
 
-    it 'shows just the class nameif it was the most recent error rasied' do
-      begin
-        raise error_class
-      rescue error_class
-        shf_error = error_class.new
-      end
-      expect(shf_error.message).to eq "SHFStepsError"
-    end
   end
 
 
@@ -49,24 +59,11 @@ RSpec.describe 'Cucumber Steps Errors' do
       expect(PagenameUnknown.new.message).to eq "PagenameUnknown: The page name is unknown.\n  You may need to add it to the list of known paths in the get_path() case statement."
     end
 
-    it 'message with the page name' do
+    it 'message includes the specific page name if one is given' do
       expect(PagenameUnknown.new(page_name: 'blorfo').message).to eq "PagenameUnknown: The page name 'blorfo' is unknown.\n  You may need to add it to the list of known paths in the get_path() case statement."
     end
 
-
-    describe 'wrapped (nested) error' do
-
-      it 'raise and show original error' do
-
-        begin
-          raise NoMethodError
-        rescue NoMethodError
-          shf_error = PagenameUnknown.new(page_name: 'blorfo')
-        end
-
-        expect(shf_error.message).to eq "PagenameUnknown: This was raised after this error: NoMethodError.: The page name 'blorfo' is unknown.\n  You may need to add it to the list of known paths in the get_path() case statement."
-      end
-    end
+    it_behaves_like 'it shows the previous error when this one is raised (nested errors)', PagenameUnknown.new(page_name: 'blorfo')
 
   end
 
@@ -77,24 +74,12 @@ RSpec.describe 'Cucumber Steps Errors' do
       expect(UnableToVisitConstructedPath.new.message).to eq "UnableToVisitConstructedPath: Unable to visit the manually constructed path.\n  You may need to add it to the list of known paths in the get_path() case statement."
     end
 
-    it 'message with the constructed path' do
+    it 'message includes the specific constructed path if one is given' do
       expect(UnableToVisitConstructedPath.new(constructed_path: 'something_blorfo_page').message).to eq "UnableToVisitConstructedPath: Unable to visit the manually constructed path 'something_blorfo_page'.\n  You may need to add it to the list of known paths in the get_path() case statement."
     end
 
+    it_behaves_like 'it shows the previous error when this one is raised (nested errors)', UnableToVisitConstructedPath.new(constructed_path: 'some_constructed_path')
 
-    describe 'wrapped (nested) error' do
-
-      it 'raise and show original error' do
-
-        begin
-          raise NoMethodError
-        rescue NoMethodError
-          shf_error = UnableToVisitConstructedPath.new(constructed_path: 'some_constructed_path')
-        end
-
-        expect(shf_error.message).to eq "UnableToVisitConstructedPath: This was raised after this error: NoMethodError.: Unable to visit the manually constructed path 'some_constructed_path'.\n  You may need to add it to the list of known paths in the get_path() case statement."
-      end
-    end
   end
 
 end
