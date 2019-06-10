b7190ac15f1b56e08b49b94d79fa5e28514c5f8d
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-06-10 16:22:16 -0700

    refactor TaskAttempt: add Successful..., Failed...

--- spec/factories/one_time_tasker/task_attempts.rb
+++ spec/factories/one_time_tasker/task_attempts.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
 
-  factory :one_time_tasker_task_attempt, class: 'OneTimeTasker::TaskAttempt' do
+  factory :one_time_tasker_task_attempt, class: OneTimeTasker::TaskAttempt do
     task_name { "task name" }
     task_source { "task source" }
     attempted_on { Time.zone.now }
-    was_successful { true }
+    was_successful { false }
     notes { '' }
   end
 
-  # cannot just use :successful because it creates a collision with
+  # cannot just use :successful because it collides with
   # the :successful trait used in the payments factory
   trait :successful_task do
     was_successful { true }
