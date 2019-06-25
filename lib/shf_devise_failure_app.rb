0e78324fb6ce500ca1525e8d051300e15fdf41f8
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-06-25 16:10:39 -0700

    keep any flash alerts about missing login or password

--- lib/shf_devise_failure_app.rb
+++ lib/shf_devise_failure_app.rb
@@ -18,7 +18,6 @@ class ShfDeviseFailureApp < Devise::FailureApp
 
 
   def route(scope)
-
     # redirect only if they are a User,  not an Admin (or other type of Devise scope)
     scope.to_sym == :user ? :new_user_session_url : super
   end
@@ -29,8 +28,35 @@ class ShfDeviseFailureApp < Devise::FailureApp
     if http_auth?
       http_auth
     else
+      flash.now[:alert] = i18n_message(:invalid) if is_flashing_format? && warden_options[:recall]
       redirect
     end
   end
+
+
+  # This differs from super because it _adds_ the i18n_message to any existing
+  # flash alerts instead of just assigning (and thus clobbering any flash
+  # alerts that might already be there). This also uses flash.keep(:alert)
+  # so that any alert message won't be lost if we redirect to a sign in page.
+  # We must add to alerts if the email or password is missing from the sign_in
+  # page.
+  #
+  def redirect
+    store_location!
+    if is_flashing_format?
+      if flash[:timedout] && flash[:alert]
+        flash.keep(:timedout)
+        flash.keep(:alert)
+      else
+        unless flash[:alert] == i18n_message
+          flash[:alert] =  "#{flash[:alert]}  #{i18n_message}"
+        end
+        flash.keep(:alert)
+      end
+    end
+    redirect_to redirect_url
+  end
+
+
 end
 
