cfac5570cf1b7efa71eae33cfc806d923dbfe407
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2019-08-06 18:54:33 -0700

    use a constant for the cookie name

--- features/step_definitions/cookies_steps.rb
+++ features/step_definitions/cookies_steps.rb
@@ -6,17 +6,17 @@
 # You can display the current cookies with
 #   puts "show_me_cookies_adapter.get_me_the_cookies: #{@show_me_cookies_adapter.get_me_the_cookies}"
 
+EU_COOKIE_NAME = 'cookie_eu_consented'
 
 # -----------------------
 # EU cookies consent (see the cookies_eu gem)
 #
 And("the EU cookies consent cookie is set to {capture_string}") do | cookie_value|
-
   curr_driver = Capybara.current_driver
   curr_session_driver = Capybara.current_session.driver
   show_me_cookies_adapter = ShowMeTheCookies.adapters[curr_driver].new(curr_session_driver)
 
-  show_me_cookies_adapter.create_cookie('cookie_eu_consented', cookie_value, {})
+  show_me_cookies_adapter.create_cookie(EU_COOKIE_NAME, cookie_value, {})
 end
 
 And("the EU cookies consent cookie is set to true") do
@@ -33,5 +33,5 @@ And("the EU cookies consent cookie does not exist") do
   curr_session_driver = Capybara.current_session.driver
   show_me_cookies_adapter = ShowMeTheCookies.adapters[curr_driver].new(curr_session_driver)
 
-  show_me_cookies_adapter.delete_cookie('cookie_eu_consented')
+  show_me_cookies_adapter.delete_cookie(EU_COOKIE_NAME)
 end
